b94a1aa1e962669fbe5fcdd0d98006ac2a9ea3f9
alexbejann <alex_bejan11@yahoo.com>
2023-05-16 14:51:28 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.givt_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:givt_app/app/app.dart';
+import 'package:givt_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
