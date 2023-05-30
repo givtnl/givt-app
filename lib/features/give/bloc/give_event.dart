c596b27afe7d6100fdb2889e4d60e47cac0e9fdf
alexbejann <alex_bejan11@yahoo.com>
2023-05-30 21:02:27 +0200

    BFF-596 Added bloc for giving flow

---
+++ lib/features/give/bloc/give_event.dart
@@ -0,0 +1,9 @@
+part of 'give_bloc.dart';
+
+abstract class GiveEvent extends Equatable {
+  const GiveEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
---
+++ lib/features/give/bloc/give_state.dart
@@ -0,0 +1,10 @@
+part of 'give_bloc.dart';
+
+abstract class GiveState extends Equatable {
+  const GiveState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class GiveInitial extends GiveState {}
