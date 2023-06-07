2d98981fcc4ee5bab9b518b1650fba37ab60a0d4
alexbejann <alex_bejan11@yahoo.com>
2023-06-07 10:55:36 +0200

    BFF-620 Added CollectGroupRepository

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:givt_app/core/network/api_service.dart';
 import 'package:givt_app/features/auth/repositories/auth_repository.dart';
 import 'package:givt_app/features/give/repositories/campaign_repository.dart';
+import 'package:givt_app/shared/repositories/collect_group_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
@@ -21,6 +22,12 @@ Future<void> init() async {
     )
     ..registerLazySingleton(
       () => CampaignRepository(
+        getIt(),
+      ),
+    )
+    ..registerLazySingleton(
+      () => CollectGroupRepository(
+        getIt(),
         getIt(),
       ),
     );
