59184f8820addf727b3dadaa590ad328ff226f06
alexbejann <alex_bejan11@yahoo.com>
2023-06-12 08:54:29 +0200

    BFF-676 Implemented needs registration dialog

--- lib/features/give/pages/home_page.dart
+++ lib/features/give/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:givt_app/features/auth/cubit/auth_cubit.dart';
@@ -42,31 +43,41 @@ class HomePage extends StatelessWidget {
       body: Stack(
         alignment: Alignment.topCenter,
         children: [
-          ChooseAmount(
-            country: auth.user.country,
-            amountLimit: auth.user.amountLimit,
-            onAmountChanged:
-                (firstCollection, secondCollection, thirdCollection) {
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (_) => BlocProvider(
-                    create: (context) => GiveBloc(
-                      getIt(),
-                      getIt(),
-                      getIt(),
-                    )..add(
-                        GiveAmountChanged(
-                          firstCollectionAmount: firstCollection,
-                          secondCollectionAmount: secondCollection,
-                          thirdCollectionAmount: thirdCollection,
-                        ),
+          Builder(
+            builder: (context) {
+              if (auth.user.needRegistration && !auth.user.mandateSigned) {
+                WidgetsBinding.instance.addPostFrameCallback(
+                  (_) => _buildNeedsRegistrationDialog(context),
+                );
+              }
+
+              return ChooseAmount(
+                country: auth.user.country,
+                amountLimit: auth.user.amountLimit,
+                onAmountChanged:
+                    (firstCollection, secondCollection, thirdCollection) {
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                      builder: (_) => BlocProvider(
+                        create: (context) => GiveBloc(
+                          getIt(),
+                          getIt(),
+                          getIt(),
+                        )..add(
+                            GiveAmountChanged(
+                              firstCollectionAmount: firstCollection,
+                              secondCollectionAmount: secondCollection,
+                              thirdCollectionAmount: thirdCollection,
+                            ),
+                          ),
+                        child: const SelectGivingWayPage(),
                       ),
-                    child: const SelectGivingWayPage(),
-                  ),
-                  fullscreenDialog: true,
-                ),
+                      fullscreenDialog: true,
+                    ),
+                  );
+                  return true;
+                },
               );
-              return true;
             },
           ),
           ColoredBox(
@@ -82,6 +93,42 @@ class HomePage extends StatelessWidget {
                 width: size.width * 0.2,
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _buildNeedsRegistrationDialog(
+    BuildContext context,
+  ) {
+    return showDialog<void>(
+      context: context,
+      builder: (_) => CupertinoAlertDialog(
+        title: Text(context.l10n.importantReminder),
+        content: Text(context.l10n.finalizeRegistrationPopupText),
+        actions: [
+          Column(
+            children: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text(context.l10n.askMeLater),
+              ),
+              const Divider(
+                height: 2,
+              ),
+              TextButton(
+                onPressed: () {
+                  //todo redirect to registration page
+                },
+                child: Text(
+                  context.l10n.finalizeRegistration,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
