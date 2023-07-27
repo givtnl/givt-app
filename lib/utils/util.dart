05ed9bb91b5771d74e2941dc9efad7fac9d120fc
alexbejann <alex_bejan11@yahoo.com>
2023-07-27 10:20:21 +0200

    BFF-976 Added fingerprint for aws api

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -3,7 +3,7 @@ import 'package:givt_app/core/enums/enums.dart';
 
 class Util {
   /// Allowed SHA-256 fingerprints for Givt backend
-  /// The list is used in APIService while building the http.client 
+  /// The list is used in APIService while building the http.client
   /// using the http_certificate_pinning package
   /// https://github.com/diefferson/http_certificate_pinning/tree/master
   static final List<String> allowedSHAFingerprints = [
@@ -18,6 +18,15 @@ class Util {
 
     /// dev-backend.givt.app
     'C6 E5 6F E7 C5 BA CE EA DD 87 7F A0 C0 DA 4D D1 EC DF 69 A6 F5 37 08 0D E3 63 D0 7D 94 66 C3 36',
+
+    /// api.production.givt.app
+    'B4 00 35 42 EF C3 19 42 25 BD A5 5F 1D FE 02 27 CB D0 05 59 9D F5 73 7D 02 2B C8 FC 68 5D B5 6C',
+
+    /// api.development.givt.app
+    'B6 BC 0A 83 BA BC 3A B4 A0 DA CF 49 2F 88 A5 BB 87 8E E2 FD 02 91 A6 51 17 B8 E3 05 B7 16 04 CF',
+
+    /// api.production.givtapp.net
+    '97 F0 E8 B1 08 59 48 D0 EC AD 82 96 20 4A 47 04 BE F5 5D D2 0F 7E AB BF 33 DF 52 FB D3 A7 4F 96',
   ];
   static final ukPostCodeRegEx = RegExp(
       r'^(([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) [0-9][A-Z]{2}$');
