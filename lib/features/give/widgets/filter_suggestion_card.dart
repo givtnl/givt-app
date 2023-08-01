a763584566efc0f342cc161ea6c175ee83a3ffc7
alexbejann <alex_bejan11@yahoo.com>
2023-08-01 12:16:27 +0200

    BFF-653 Moved visibility inside the filtersuggestion card

--- lib/features/give/widgets/filter_suggestion_card.dart
+++ lib/features/give/widgets/filter_suggestion_card.dart
@@ -8,70 +8,79 @@ class FilterSuggestionCard extends StatelessWidget {
     required this.activeIcon,
     required this.icon,
     required this.onTap,
+    this.visible = true,
+    this.width = 85,
+    this.height = 80,
     super.key,
   });
 
+  final bool visible;
   final bool isFocused;
   final Color color;
   final String title;
   final String activeIcon;
   final String icon;
+  final double width;
+  final double height;
   final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return ConstrainedBox(
-      constraints: const BoxConstraints.expand(
-        width: 85,
-        height: 80,
-      ),
-      child: InkWell(
-        onTap: onTap,
-        child: Card(
-          color: color,
-          child: Container(
-            padding: const EdgeInsets.only(left: 5, right: 5),
-            decoration: BoxDecoration(
-              color: isFocused ? Colors.white : color,
-              borderRadius: BorderRadius.circular(3.5),
-              border: Border(
-                top: BorderSide(
-                  color: isFocused ? color : Colors.transparent,
-                ),
-                bottom: BorderSide(
-                  color: isFocused ? color : Colors.transparent,
-                  width: 5,
-                ),
-                left: BorderSide(
-                  color: isFocused ? color : Colors.transparent,
-                ),
-                right: BorderSide(
-                  color: isFocused ? color : Colors.transparent,
+    return Visibility(
+      visible: visible,
+      child: ConstrainedBox(
+        constraints: BoxConstraints.expand(
+          width: width,
+          height: height,
+        ),
+        child: InkWell(
+          onTap: onTap,
+          child: Card(
+            color: color,
+            child: Container(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              decoration: BoxDecoration(
+                color: isFocused ? Colors.white : color,
+                borderRadius: BorderRadius.circular(3.5),
+                border: Border(
+                  top: BorderSide(
+                    color: isFocused ? color : Colors.transparent,
+                  ),
+                  bottom: BorderSide(
+                    color: isFocused ? color : Colors.transparent,
+                    width: 5,
+                  ),
+                  left: BorderSide(
+                    color: isFocused ? color : Colors.transparent,
+                  ),
+                  right: BorderSide(
+                    color: isFocused ? color : Colors.transparent,
+                  ),
                 ),
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 5,
+                    ),
+                    child: Image.asset(
+                      isFocused ? activeIcon : icon,
+                      width: size.width * (isFocused ? 0.08 : 0.06),
+                    ),
                   ),
-                  child: Image.asset(
-                    isFocused ? activeIcon : icon,
-                    width: size.width * (isFocused ? 0.08 : 0.06),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: isFocused ? color : Colors.white,
+                        ),
                   ),
-                ),
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: isFocused ? color : Colors.white,
-                      ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
