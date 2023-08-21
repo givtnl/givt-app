2849ef6d2636b1d8dc7e67704801c79280b1448e
alexbejann <alex_bejan11@yahoo.com>
2023-08-21 09:14:37 +0200

    BFF-653 Added discover options

--- lib/features/give/widgets/choose_category.dart
+++ lib/features/give/widgets/choose_category.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:givt_app/core/enums/enums.dart';
+import 'package:givt_app/features/give/widgets/widgets.dart';
 
 class ChooseCategory extends StatelessWidget {
   const ChooseCategory({super.key});
@@ -6,12 +10,106 @@ class ChooseCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Container(
-        height: size.height,
+
+    return ConstrainedBox(
+      constraints: BoxConstraints.expand(
         width: size.width,
-        child: Text('Choose Category'),
+        height: size.height,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: size.height * 0.1,
+                  child: FilterSuggestionCard(
+                    isFocused: false,
+                    color: CollecGroupType.artists.color,
+                    title: CollecGroupType.artists.name,
+                    activeIcon: CollecGroupType.artists.activeIcon,
+                    icon: CollecGroupType.artists.icon,
+                    width: size.width * 0.4,
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FilterSuggestionCard(
+                  isFocused: false,
+                  color: CollecGroupType.artists.color,
+                  title: CollecGroupType.artists.name,
+                  activeIcon: CollecGroupType.artists.activeIcon,
+                  icon: CollecGroupType.artists.icon,
+                  width: size.width * 0.4,
+                  height: size.height * 0.1,
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FilterSuggestionCard(
+                  isFocused: false,
+                  color: CollecGroupType.charities.color,
+                  title: CollecGroupType.charities.name,
+                  activeIcon: CollecGroupType.charities.activeIcon,
+                  icon: CollecGroupType.charities.icon,
+                  width: size.width * 0.4,
+                  height: size.height * 0.2,
+                  onTap: () {},
+                ),
+              ),
+              Expanded(
+                child: FilterSuggestionCard(
+                  isFocused: false,
+                  color: CollecGroupType.church.color,
+                  title: CollecGroupType.church.name,
+                  activeIcon: CollecGroupType.church.activeIcon,
+                  icon: CollecGroupType.church.icon,
+                  width: size.width * 0.4,
+                  height: size.height * 0.2,
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FilterSuggestionCard(
+                  isFocused: false,
+                  color: CollecGroupType.campaign.color,
+                  title: CollecGroupType.campaign.name,
+                  activeIcon: CollecGroupType.campaign.activeIcon,
+                  icon: CollecGroupType.campaign.icon,
+                  width: size.width * 0.4,
+                  height: size.height * 0.2,
+                  onTap: () {},
+                ),
+              ),
+              Expanded(
+                child: FilterSuggestionCard(
+                  visible: Platform.isIOS,
+                  isFocused: false,
+                  color: CollecGroupType.artists.color,
+                  title: CollecGroupType.artists.name,
+                  activeIcon: CollecGroupType.artists.activeIcon,
+                  icon: CollecGroupType.artists.icon,
+                  width: size.width * 0.4,
+                  height: size.height * 0.2,
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
