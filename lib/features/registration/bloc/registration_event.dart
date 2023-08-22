9e824dc90d87e155ce07bd86ce9a3018b2bc5ed7
Daniela Damaschin <whateverellie@gmail.com>
2023-08-22 15:14:42 +0200

    KIDS 1176 PR feedback improvements

--- lib/features/registration/bloc/registration_event.dart
+++ lib/features/registration/bloc/registration_event.dart
@@ -2,9 +2,6 @@ part of 'registration_bloc.dart';
 
 abstract class RegistrationEvent extends Equatable {
   const RegistrationEvent();
-
-  @override
-  List<Object> get props => [];
 }
 
 class RegistrationPasswordSubmitted extends RegistrationEvent {
