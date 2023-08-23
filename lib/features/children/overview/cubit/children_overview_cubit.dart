1afff5b6e662f9c17153ee990bee56a56ea96198
Pavlo Pavelko <p.pavelko.job@gmail.com>
2023-08-23 13:43:06 +0300

    Removed temp code

--- lib/features/children/overview/cubit/children_overview_cubit.dart
+++ lib/features/children/overview/cubit/children_overview_cubit.dart
@@ -3,7 +3,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:givt_app/features/children/overview/models/profile.dart';
-import 'package:givt_app/features/children/overview/models/wallet.dart';
 import 'package:givt_app/features/children/overview/repositories/children_overview_repository.dart';
 
 part 'children_overview_state.dart';
@@ -22,71 +21,7 @@ class ChildrenOverviewCubit extends Cubit<ChildrenOverviewState> {
           await _childrenOverviewRepository.fetchChildren(parentGuid);
       emit(ChildrenOverviewUpdatedState(profiles: response));
     } catch (error) {
-      final fakeChildren = <Profile>[];
-
-      fakeChildren.add(
-        const Profile(
-          id: '',
-          firstName: 'Pavlo',
-          lastName: '',
-          nickname: '',
-          comment: '',
-          wallet: Wallet(balance: 0, total: 42, pending: 0, currency: ''),
-          pictureURL: '',
-        ),
-      );
-
-      fakeChildren.add(
-        const Profile(
-          id: '',
-          firstName: 'Emma',
-          lastName: '',
-          nickname: '',
-          comment: '',
-          wallet: Wallet(balance: 0, total: 99, pending: -25, currency: ''),
-          pictureURL: '',
-        ),
-      );
-
-      fakeChildren.add(
-        const Profile(
-          id: '',
-          firstName: 'John',
-          lastName: '',
-          nickname: '',
-          comment: '',
-          wallet: Wallet(balance: 0, total: 20, pending: -4, currency: ''),
-          pictureURL: '',
-        ),
-      );
-
-      fakeChildren.add(
-        const Profile(
-          id: '',
-          firstName: 'Pedro',
-          lastName: '',
-          nickname: '',
-          comment: '',
-          wallet: Wallet(balance: 0, total: 120, pending: 54, currency: ''),
-          pictureURL: '',
-        ),
-      );
-
-      fakeChildren.add(
-        const Profile(
-          id: '',
-          firstName: 'Bob',
-          lastName: '',
-          nickname: '',
-          comment: '',
-          wallet: Wallet(balance: 0, total: 89, pending: 10, currency: ''),
-          pictureURL: '',
-        ),
-      );
-
-      emit(ChildrenOverviewUpdatedState(profiles: fakeChildren));
-
-      // emit(ChildrenOverviewErrorState(errorMessage: error.toString()));
+      emit(ChildrenOverviewErrorState(errorMessage: error.toString()));
     }
   }
 }
