740dec564f716cec6b52e1af837b8a7fe514983f
alexbejann <alex_bejan11@yahoo.com>
2023-09-06 12:21:03 +0300

    BFF-1097 Added firebase configs into the bootstrap method

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_core/firebase_core.dart' show Firebase;
-import 'package:flutter/material.dart';
 import 'package:givt_app/app/app.dart';
 import 'package:givt_app/app/bootstrap.dart';
 import 'package:givt_app/app/firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+void main() {
+  bootstrap(
     name: 'givtapp-ebde1',
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await bootstrap(
     builder: () => const App(),
   );
 }
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,8 +1,11 @@
 import 'package:givt_app/app/app.dart';
 import 'package:givt_app/app/bootstrap.dart';
+import 'package:givt_app/app/firebase_options_dev.dart';
 
 void main() {
   bootstrap(
+    name: 'givt-dev-pre',
+    options: DefaultFirebaseOptions.currentPlatform,
     builder: () => const App(),
   );
 }
