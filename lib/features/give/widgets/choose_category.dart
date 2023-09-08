4b6aac8d5f9fc70b44a39bc18ff93536ed3df528
alexbejann <alex_bejan11@yahoo.com>
2023-09-08 14:31:05 +0300

    BFF-653 Added search and all card

--- lib/features/give/widgets/choose_category.dart
+++ lib/features/give/widgets/choose_category.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/core/enums/enums.dart';
 import 'package:givt_app/features/give/widgets/widgets.dart';
+import 'package:givt_app/l10n/l10n.dart';
 
 class ChooseCategory extends StatelessWidget {
   const ChooseCategory({super.key});
@@ -10,6 +12,7 @@ class ChooseCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
+    final locals = context.l10n;
 
     return ConstrainedBox(
       constraints: BoxConstraints.expand(
@@ -17,35 +20,27 @@ class ChooseCategory extends StatelessWidget {
         height: size.height,
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(
+            height: size.height * 0.05,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                child: SizedBox(
-                  width: size.height * 0.1,
-                  child: FilterSuggestionCard(
-                    isFocused: false,
-                    color: CollecGroupType.artists.color,
-                    title: CollecGroupType.artists.name,
-                    activeIcon: CollecGroupType.artists.activeIcon,
-                    icon: CollecGroupType.artists.icon,
-                    width: size.width * 0.4,
-                    onTap: () {},
-                  ),
+                child: _buildSearchSeeAllCard(
+                  context,
+                  title: locals.discoverSearchButton,
+                  icon: FontAwesomeIcons.magnifyingGlass,
+                  onPressed: () {},
                 ),
               ),
               Expanded(
-                child: FilterSuggestionCard(
-                  isFocused: false,
-                  color: CollecGroupType.artists.color,
-                  title: CollecGroupType.artists.name,
-                  activeIcon: CollecGroupType.artists.activeIcon,
-                  icon: CollecGroupType.artists.icon,
-                  width: size.width * 0.4,
-                  height: size.height * 0.1,
-                  onTap: () {},
+                child: _buildSearchSeeAllCard(
+                  context,
+                  title: locals.discoverDiscoverButton,
+                  icon: FontAwesomeIcons.compass,
+                  onPressed: () {},
                 ),
               ),
             ],
@@ -61,7 +56,7 @@ class ChooseCategory extends StatelessWidget {
                   activeIcon: CollecGroupType.charities.activeIcon,
                   icon: CollecGroupType.charities.icon,
                   width: size.width * 0.4,
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                   onTap: () {},
                 ),
               ),
@@ -73,7 +68,7 @@ class ChooseCategory extends StatelessWidget {
                   activeIcon: CollecGroupType.church.activeIcon,
                   icon: CollecGroupType.church.icon,
                   width: size.width * 0.4,
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                   onTap: () {},
                 ),
               ),
@@ -90,7 +85,7 @@ class ChooseCategory extends StatelessWidget {
                   activeIcon: CollecGroupType.campaign.activeIcon,
                   icon: CollecGroupType.campaign.icon,
                   width: size.width * 0.4,
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                   onTap: () {},
                 ),
               ),
@@ -103,7 +98,7 @@ class ChooseCategory extends StatelessWidget {
                   activeIcon: CollecGroupType.artists.activeIcon,
                   icon: CollecGroupType.artists.icon,
                   width: size.width * 0.4,
-                  height: size.height * 0.2,
+                  height: size.height * 0.3,
                   onTap: () {},
                 ),
               ),
@@ -113,4 +108,40 @@ class ChooseCategory extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildSearchSeeAllCard(
+    BuildContext context, {
+    required VoidCallback onPressed,
+    required String title,
+    required IconData icon,
+  }) =>
+      Padding(
+        padding: const EdgeInsets.all(5),
+        child: OutlinedButton(
+          onPressed: onPressed,
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 15,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  icon,
+                ),
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 }
