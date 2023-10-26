20602f0a4d490e9c45ccdbe31286275f4501a0cf
Daniela Damaschin <whateverellie@gmail.com>
2023-10-26 18:56:42 +0200

    Float all pending to top

--- lib/features/children/family_history/family_history_cubit/family_history_cubit.dart
+++ lib/features/children/family_history/family_history_cubit/family_history_cubit.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:givt_app/features/children/family_history/models/child_donation.dart';
+import 'package:givt_app/features/children/family_history/models/child_donation_helper.dart';
 import 'package:givt_app/features/children/family_history/repository/family_history_repository.dart';
 import 'package:givt_app/features/children/family_history/models/history_item.dart';
 
@@ -15,29 +17,64 @@ class FamilyHistoryCubit extends Cubit<FamilyHistoryState> {
     emit(state.copyWith(status: HistroryStatus.loading));
 
     try {
-      final history = <HistoryItem>[];
-      // ignore: cascade_invocations
-      history.addAll(state.history);
+      final temporaryHistory = <HistoryItem>[];
+      final arrangedHistory = <HistoryItem>[];
+      final pendingHistory = <HistoryItem>[];
+      final decidedHistory = <HistoryItem>[];
+
+      if (state.history.isNotEmpty) {
+        final previousDonation = <HistoryItem>[];
+        final previousPending = <HistoryItem>[];
+        previousDonation.addAll(state.history
+            .where((element) => element.type == HistoryTypes.donation));
+        previousPending.addAll(previousDonation.where((element) {
+          final e = element as ChildDonation;
+          return e.state == DonationState.pending;
+        }));
+        final previousDecided = state.history
+            .where((element) => !previousPending.contains(element));
+        temporaryHistory.addAll(previousDecided);
+        pendingHistory.addAll(previousPending);
+      }
+
       // fetch donations
       final donationHistory = await historyRepo.fetchHistory(
           pageNumber: state.pageNr, type: HistoryTypes.donation);
-      history.addAll(donationHistory);
+
+      pendingHistory.addAll(
+        donationHistory.where((element) {
+          final e = element as ChildDonation;
+          return e.state == DonationState.pending;
+        }),
+      );
+      decidedHistory.addAll(
+        donationHistory.where((element) {
+          final e = element as ChildDonation;
+          return e.state != DonationState.pending;
+        }),
+      );
+      temporaryHistory.addAll(decidedHistory);
       // fetch allowances
       final allowanceHistory = await historyRepo.fetchHistory(
           pageNumber: state.pageNr, type: HistoryTypes.allowance);
-      history.addAll(allowanceHistory);
+      temporaryHistory.addAll(allowanceHistory);
       // sort from newest to oldest
-      history.sort((a, b) => b.date.compareTo(a.date));
+      pendingHistory.sort((a, b) => b.date.compareTo(a.date));
+      temporaryHistory.sort((a, b) => b.date.compareTo(a.date));
+      arrangedHistory
+        ..addAll(pendingHistory)
+        ..addAll(temporaryHistory);
       // check if they reached end of history
       // if end of history do not increment page nr
       if (donationHistory.isEmpty && allowanceHistory.isEmpty) {
-        emit(state.copyWith(status: HistroryStatus.loaded, history: history));
+        emit(state.copyWith(
+            status: HistroryStatus.loaded, history: arrangedHistory));
         return;
       }
       // update state
       emit(state.copyWith(
           status: HistroryStatus.loaded,
-          history: history,
+          history: arrangedHistory,
           pageNr: state.pageNr + 1));
     } catch (e) {
       emit(state.copyWith(status: HistroryStatus.error, error: e.toString()));
