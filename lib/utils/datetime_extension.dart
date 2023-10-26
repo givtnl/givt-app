db1d030647a82ea74466e467ad690e55f9bdbe6c
Daniela Damaschin <whateverellie@gmail.com>
2023-10-26 13:35:39 +0200

    yesterday & today localization of strings

--- lib/utils/datetime_extension.dart
+++ lib/utils/datetime_extension.dart
@@ -1,20 +1,21 @@
+import 'package:givt_app/l10n/l10n.dart';
 import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
-  String formatDate() {
-    DateTime now = DateTime.now();
+  String formatDate(AppLocalizations locals) {
+    final now = DateTime.now();
 
     // Check if the date is today
     if (year == now.year && month == now.month && day == now.day) {
-      return 'Today';
+      return locals.today;
     }
 
     // Check if the date is yesterday
-    DateTime yesterday = now.subtract(const Duration(days: 1));
+    final yesterday = now.subtract(const Duration(days: 1));
     if (year == yesterday.year &&
         month == yesterday.month &&
         day == yesterday.day) {
-      return 'Yesterday';
+      return locals.yesterday;
     }
 
     final formatter = DateFormat('MM/dd');
