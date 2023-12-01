41503694248e44672b2d84f4f43372b0590dfd44
alexbejann <alex_bejan11@yahoo.com>
2023-12-01 18:37:48 +0100

    BFF-1478 Added update dialog

--- lib/shared/models/models.dart
+++ lib/shared/models/models.dart
@@ -1,8 +1,9 @@
+export 'app_update.dart';
 export 'collect_group.dart';
 export 'location.dart';
-export 'summary_item.dart';
 export 'multi_use_allocation.dart';
 export 'qr_code.dart';
 export 'stripe_response.dart';
+export 'summary_item.dart';
 export 'temp_user.dart';
 export 'user_ext.dart';
