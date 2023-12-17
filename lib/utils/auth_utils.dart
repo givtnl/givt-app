e52f8fccd810fd04baa0e400a097297f389692c6
alexbejann <alex_bejan11@yahoo.com>
2023-12-17 12:42:13 +0100

    BFF-1485 Don't navigate to the home page if the page is already there

--- lib/utils/auth_utils.dart
+++ lib/utils/auth_utils.dart
@@ -38,12 +38,7 @@ class AuthUtils {
       }
       await context.read<AuthCubit>().refreshSession();
 
-      /// Because the session is refreshed and the router needs to respond
-      /// we will delay the navigation by 100 milliseconds.
-      Future.delayed(
-        const Duration(milliseconds: 100),
-        navigate,
-      );
+      navigate();
     } on PlatformException catch (e) {
       await LoggingInfo.instance.info(
         'Error while authenticating with biometrics: ${e.message}',
