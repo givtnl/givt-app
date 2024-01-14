af63d4bbee7a5281e9e232a00a265c36f096f302
Pavlo Pavelko <p.pavelko.job@gmail.com>
2024-01-14 17:33:03 +0200

    Download g4k floating action button is added to the family overview page

---
+++ lib/features/children/overview/widgets/download_givt_for_kids_app_widget.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:givt_app/utils/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DownloadGivtForKidsAppWidget extends StatelessWidget {
+  const DownloadGivtForKidsAppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final appId = Platform.isAndroid ? 'net.givtapp.kids' : '1658797002';
+        final url = Uri.parse(
+          Platform.isAndroid
+              ? 'market://details?id=$appId'
+              : 'https://apps.apple.com/app/id$appId',
+        );
+        launchUrl(
+          url,
+          mode: LaunchMode.externalApplication,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: AppTheme.givtBlue,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(6)),
+              child: SvgPicture.asset(
+                'assets/images/givt4kids_logo.svg',
+                height: 40,
+                width: 40,
+              ),
+            ),
+            const SizedBox(width: 16),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Download Givt4Kids',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'With this app your kids can start making a difference!',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold,
+                        ),
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
