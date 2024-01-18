52d9d2c388ee7987bec9bf887bf0a15db6998958
Pavlo Pavelko <p.pavelko.job@gmail.com>
2024-01-18 19:30:30 +0200

    code review improvements

--- lib/features/children/parental_approval/dialogs/parental_approval_dialog.dart
+++ lib/features/children/parental_approval/dialogs/parental_approval_dialog.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:givt_app/app/injection/injection.dart';
 import 'package:givt_app/features/children/family_history/models/child_donation.dart';
 import 'package:givt_app/features/children/parental_approval/cubit/parental_approval_cubit.dart';
-import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_approved_page.dart';
-import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_confirmation_page.dart';
-import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_declined_page.dart';
-import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_error_page.dart';
-import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_loading_page.dart';
-import 'package:go_router/go_router.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_dialog_content.dart';
 
 class ParentalApprovalDialog extends StatelessWidget {
   const ParentalApprovalDialog({
@@ -19,29 +13,6 @@ class ParentalApprovalDialog extends StatelessWidget {
 
   final ChildDonation donation;
 
-  Widget _createPage(DecisionStatus status) {
-    switch (status) {
-      case DecisionStatus.confirmation:
-        return ParentalApprovalConfirmationPage(
-          donation: donation,
-        );
-      case DecisionStatus.loading:
-        return const ParentalApprovalLoadingPage();
-      case DecisionStatus.approved:
-        return ParentalApprovalApprovedPage(
-          donation: donation,
-        );
-      case DecisionStatus.declined:
-        return ParentalApprovalDeclinedPage(
-          donation: donation,
-        );
-      case DecisionStatus.error:
-        return const ParentalApprovalErrorPage();
-      case DecisionStatus.pop:
-        return Container();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -50,49 +21,16 @@ class ParentalApprovalDialog extends StatelessWidget {
         donation: donation,
         decisionRepository: getIt(),
       ),
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: size.width * 0.1,
-        ),
-        child: Center(
-          child: BlocConsumer<ParentalApprovalCubit, ParentalApprovalState>(
-            listener: (context, state) {
-              if (state.status == DecisionStatus.pop) {
-                context.pop(state.decisionMade);
-              }
-            },
-            builder: (context, state) {
-              return Stack(
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    elevation: 7,
-                    child: Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      child: _createPage(state.status),
-                    ),
-                  ),
-                  if (state.status == DecisionStatus.confirmation)
-                    Positioned(
-                      top: 20,
-                      right: 20,
-                      child: GestureDetector(
-                        onTap: () => context.pop(false),
-                        child: SvgPicture.asset(
-                          'assets/images/close_icon.svg',
-                          alignment: Alignment.centerRight,
-                        ),
-                      ),
-                    ),
-                ],
-              );
-            },
+      child: Center(
+        child: SizedBox.square(
+          dimension: size.width * 0.8,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+            color: Colors.white,
+            elevation: 7,
+            child: ParentalApprovalDialogContent(donation: donation),
           ),
         ),
       ),
---
+++ lib/features/children/parental_approval/widgets/parental_approval_dialog_content.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:givt_app/features/auth/cubit/auth_cubit.dart';
+import 'package:givt_app/features/children/family_history/family_history_cubit/family_history_cubit.dart';
+import 'package:givt_app/features/children/family_history/models/child_donation.dart';
+import 'package:givt_app/features/children/overview/cubit/family_overview_cubit.dart';
+import 'package:givt_app/features/children/parental_approval/cubit/parental_approval_cubit.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_approved_page.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_confirmation_page.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_declined_page.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_error_page.dart';
+import 'package:givt_app/features/children/parental_approval/widgets/parental_approval_loading_page.dart';
+import 'package:go_router/go_router.dart';
+
+class ParentalApprovalDialogContent extends StatelessWidget {
+  const ParentalApprovalDialogContent({
+    required this.donation,
+    super.key,
+  });
+
+  final ChildDonation donation;
+
+  void _refreshHistory(BuildContext context) {
+    context.read<FamilyHistoryCubit>().fetchHistory(fromScratch: true);
+    context
+        .read<FamilyOverviewCubit>()
+        .fetchChildren(context.read<AuthCubit>().state.user.guid);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ParentalApprovalCubit, ParentalApprovalState>(
+      listener: (context, state) {
+        if (state.status == DecisionStatus.approved ||
+            state.status == DecisionStatus.declined) {
+          _refreshHistory(context);
+        } else if (state.status == DecisionStatus.pop) {
+          context.pop();
+        }
+      },
+      builder: (context, state) {
+        return Stack(
+          children: [
+            SizedBox.expand(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: _createPage(state.status),
+              ),
+            ),
+            if (state.status != DecisionStatus.loading &&
+                state.status != DecisionStatus.pop)
+              Positioned(
+                top: 10,
+                right: 15,
+                child: GestureDetector(
+                  onTap: () => context.pop(),
+                  child: SvgPicture.asset(
+                    'assets/images/close_icon.svg',
+                    alignment: Alignment.centerRight,
+                  ),
+                ),
+              ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _createPage(DecisionStatus status) {
+    switch (status) {
+      case DecisionStatus.confirmation:
+        return ParentalApprovalConfirmationPage(
+          donation: donation,
+        );
+      case DecisionStatus.loading:
+        return const ParentalApprovalLoadingPage();
+      case DecisionStatus.approved:
+        return ParentalApprovalApprovedPage(
+          donation: donation,
+        );
+      case DecisionStatus.declined:
+        return ParentalApprovalDeclinedPage(
+          donation: donation,
+        );
+      case DecisionStatus.error:
+        return const ParentalApprovalErrorPage();
+      case DecisionStatus.pop:
+        return Container();
+    }
+  }
+}
