27d2eb604f1a8ac9827fe938962ebe87f2116a60
Pavlo Pavelko <p.pavelko.job@gmail.com>
2024-02-09 14:40:08 +0200

    Dialog layout fixed by making background size bigger

--- lib/features/children/cached_members/dialogs/cached_members_dialog.dart
+++ lib/features/children/cached_members/dialogs/cached_members_dialog.dart
@@ -8,8 +8,9 @@ class CachedMembersDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Center(
-      child: SizedBox.square(
-        dimension: size.width * 0.8,
+      child: SizedBox(
+        width: size.width * 0.85,
+        height: size.width * 0.95,
         child: Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(25),
