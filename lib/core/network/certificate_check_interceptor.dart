5b790bd83dbb3007b25a0aa96c3d63cccd466209
daniela <whateverellie@gmail.com>
2024-02-16 17:43:35 +0100

    Remove certificate pinning

--- lib/core/network/certificate_check_interceptor.dart
+++ lib/core/network/certificate_check_interceptor.dart
@@ -10,31 +10,32 @@ import 'package:http_interceptor/http_interceptor.dart';
 class CertificateCheckInterceptor extends InterceptorContract {
   @override
   Future<BaseRequest> interceptRequest({required BaseRequest request}) async {
-    if (Platform.isIOS) {
-      return request;
-    }
-
-    /// Don't check for certificate if there is no internet connection
-    if (await getIt<NetworkInfo>().isConnected == false) {
-      return request;
-    }
-
-    final secure = await HttpCertificatePinning.check(
-      serverURL: request.url.toString(),
-      sha: SHA.SHA256,
-      allowedSHAFingerprints: Util.allowedSHAFingerprints,
-      timeout: 50,
-      index: 2,
-    );
-
-    if (!secure.contains('CONNECTION_SECURE')) {
-      await LoggingInfo.instance.error(
-        request.url.toString(),
-        methodName: StackTrace.current.toString(),
-      );
-      throw Exception('CONNECTION_NOT_SECURE');
-    }
     return request;
+    // if (Platform.isIOS) {
+    //   return request;
+    // }
+
+    // /// Don't check for certificate if there is no internet connection
+    // if (await getIt<NetworkInfo>().isConnected == false) {
+    //   return request;
+    // }
+
+    // final secure = await HttpCertificatePinning.check(
+    //   serverURL: request.url.toString(),
+    //   sha: SHA.SHA256,
+    //   allowedSHAFingerprints: Util.allowedSHAFingerprints,
+    //   timeout: 50,
+    //   index: 2,
+    // );
+
+    // if (!secure.contains('CONNECTION_SECURE')) {
+    //   await LoggingInfo.instance.error(
+    //     request.url.toString(),
+    //     methodName: StackTrace.current.toString(),
+    //   );
+    //   throw Exception('CONNECTION_NOT_SECURE');
+    // }
+    // return request;
   }
 
   @override
