6aef71da3b090cb2bca78c4dc5cbfe7cb89abf57
daniela <whateverellie@gmail.com>
2024-02-18 18:46:23 +0100

    make whole area tapable

--- lib/features/children/family_goal_tracker/pages/family_goal_tracker.dart
+++ lib/features/children/family_goal_tracker/pages/family_goal_tracker.dart
@@ -1,54 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:givt_app/app/routes/pages.dart';
 import 'package:givt_app/features/children/family_goal_tracker/cubit/goal_tracker_cubit.dart';
 import 'package:givt_app/features/children/family_goal_tracker/widgets/goal_active_widget.dart';
 import 'package:givt_app/features/children/family_goal_tracker/widgets/goal_completed_widget.dart';
 import 'package:givt_app/features/children/family_goal_tracker/widgets/no_goal_set_widget.dart';
+import 'package:givt_app/features/children/overview/cubit/family_overview_cubit.dart';
 import 'package:givt_app/utils/app_theme.dart';
 import 'package:givt_app/utils/snack_bar_helper.dart';
+import 'package:go_router/go_router.dart';
 
 class FamilyGoalTracker extends StatelessWidget {
   const FamilyGoalTracker({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.all(24),
-      decoration: ShapeDecoration(
-        color: AppTheme.primary98,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: () {
+        if (context.read<GoalTrackerCubit>().state.status ==
+            GoalTrackerStatus.noGoalSet) {
+          context.pushNamed(
+            Pages.createFamilyGoal.name,
+            extra: context.read<FamilyOverviewCubit>(),
+          );
+        }
+      },
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(24),
+        decoration: ShapeDecoration(
+          color: AppTheme.primary98,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
         ),
-      ),
-      child: BlocConsumer<GoalTrackerCubit, GoalTrackerState>(
-        listener: (context, state) {
-          if (state.status == GoalTrackerStatus.error) {
-            SnackBarHelper.showMessage(
-              context,
-              text: state.error,
-              isError: true,
-            );
-          }
-        },
-        builder: (context, state) {
-          switch (state.status) {
-            case GoalTrackerStatus.loading:
-              return const Padding(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 32),
-                child: Center(child: CircularProgressIndicator()),
+        child: BlocConsumer<GoalTrackerCubit, GoalTrackerState>(
+          listener: (context, state) {
+            if (state.status == GoalTrackerStatus.error) {
+              SnackBarHelper.showMessage(
+                context,
+                text: state.error,
+                isError: true,
               );
-            case GoalTrackerStatus.noGoalSet:
-              return const NoGoalSetWidget();
-            case GoalTrackerStatus.activeGoal:
-              return const GoalActiveWidget();
-            case GoalTrackerStatus.completedGoal:
-              return const GoalCompletedWidget();
-            // ignore: no_default_cases
-            default:
-              return const NoGoalSetWidget();
-          }
-        },
+            }
+          },
+          builder: (context, state) {
+            switch (state.status) {
+              case GoalTrackerStatus.loading:
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 32),
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              case GoalTrackerStatus.noGoalSet:
+                return const NoGoalSetWidget();
+              case GoalTrackerStatus.activeGoal:
+                return const GoalActiveWidget();
+              case GoalTrackerStatus.completedGoal:
+                return const GoalCompletedWidget();
+              // ignore: no_default_cases
+              default:
+                return const NoGoalSetWidget();
+            }
+          },
+        ),
       ),
     );
   }
--- lib/features/children/family_goal_tracker/widgets/no_goal_set_widget.dart
+++ lib/features/children/family_goal_tracker/widgets/no_goal_set_widget.dart
@@ -1,69 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:givt_app/app/routes/pages.dart';
-import 'package:givt_app/features/children/overview/cubit/family_overview_cubit.dart';
-import 'package:go_router/go_router.dart';
 
 class NoGoalSetWidget extends StatelessWidget {
   const NoGoalSetWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => context.pushNamed(
-        Pages.createFamilyGoal.name,
-        extra: context.read<FamilyOverviewCubit>(),
-      ),
-      child: Stack(
-        children: [
-          const Positioned(
-            right: 0,
-            top: 38,
-            child: Padding(
-              padding: EdgeInsets.all(12),
-              child: Icon(
-                FontAwesomeIcons.arrowRight,
-                size: 20,
-              ),
+    return Stack(
+      children: [
+        const Positioned(
+          right: 0,
+          top: 38,
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Icon(
+              FontAwesomeIcons.arrowRight,
+              size: 20,
             ),
           ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 32),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SvgPicture.asset(
-                    'assets/images/goal_flag_small.svg',
-                    width: 40,
-                    height: 40,
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'Create a Family Goal',
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                          fontSize: 17,
-                          fontFamily: 'Mulish',
-                          fontWeight: FontWeight.w800,
-                        ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'Give together',
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          fontFamily: 'Mulish',
-                          fontWeight: FontWeight.w400,
-                        ),
-                  ),
-                ],
-              ),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/images/goal_flag_small.svg',
+                  width: 40,
+                  height: 40,
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  'Create a Family Goal',
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        fontSize: 17,
+                        fontFamily: 'Mulish',
+                        fontWeight: FontWeight.w800,
+                      ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  'Give together',
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        fontFamily: 'Mulish',
+                        fontWeight: FontWeight.w400,
+                      ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
