6c32cacecdb5b17e2a41a929322270aed6585850
Pavlo Pavelko <p.pavelko.job@gmail.com>
2024-03-25 12:47:38 +0200

    code review improvement

--- lib/features/give/dialogs/give_to_family_goal_loading_dialog.dart
+++ lib/features/give/dialogs/give_to_family_goal_loading_dialog.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:givt_app/features/give/bloc/bloc.dart';
 import 'package:givt_app/l10n/l10n.dart';
-import 'package:givt_app/utils/app_theme.dart';
 import 'package:go_router/go_router.dart';
 
 class GiveToFamilyGoalLoadingDialog extends StatelessWidget {
@@ -20,16 +19,21 @@ class GiveToFamilyGoalLoadingDialog extends StatelessWidget {
           context.pop();
         }
       },
-      child: SimpleDialog(
-        backgroundColor: AppTheme.givtGraycece,
-        children: [
-          SizedBox.square(
-            dimension: size.width * 0.5,
+      child: Center(
+        child: SizedBox(
+          width: size.width * 0.85,
+          height: size.width * 0.95,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+            color: Colors.white,
+            elevation: 7,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const CircularProgressIndicator.adaptive(),
+                const CircularProgressIndicator(),
                 const SizedBox(height: 16),
                 Text(
                   context.l10n.loadingTitle,
@@ -38,7 +42,7 @@ class GiveToFamilyGoalLoadingDialog extends StatelessWidget {
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
