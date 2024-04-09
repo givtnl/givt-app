c2ec3e91c51590e77b2846cb50a1cfdfacf608de
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-04-09 14:16:32 +0200

    Migrate Action Container and Givt Elevated button from G4K

--- lib/features/children/generosity_challenge/pages/generosity_challenge_active_day.dart
+++ lib/features/children/generosity_challenge/pages/generosity_challenge_active_day.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/features/children/generosity_challenge/cubit/generosity_challenge_cubit.dart';
+import 'package:givt_app/shared/widgets/givt_elevated_button.dart';
+import 'package:givt_app/utils/utils.dart';
 
 class GenerosityChallengeActiveDay extends StatelessWidget {
   const GenerosityChallengeActiveDay({super.key});
@@ -10,23 +14,34 @@ class GenerosityChallengeActiveDay extends StatelessWidget {
     final challenge = context.read<GenerosityChallengeCubit>();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Day ${challenge.state.activeDayIndex + 1}'),
+        title: Text(
+          'Day ${challenge.state.activeDayIndex + 1}',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                color: AppTheme.givtGreen40,
+                fontFamily: 'Rouna',
+                fontWeight: FontWeight.w700,
+              ),
+        ),
         leading: BackButton(
           onPressed: challenge.overview,
+          color: AppTheme.givtGreen40,
         ),
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: AppTheme.givtLightBackgroundGreen,
+          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+        ),
+        backgroundColor: AppTheme.givtLightBackgroundGreen,
+        elevation: 0,
+        centerTitle: true,
       ),
+      // body: SafeArea(child: GenerosityDailyCard()),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(left: 35, right: 35, bottom: 30),
-        child: ElevatedButton(
-          onPressed: challenge.completeActiveDay,
-          child: Text(
-            'Complete',
-            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                  color: Colors.white,
-                ),
-          ),
-        ),
+      floatingActionButton: GivtElevatedButton(
+        onTap: challenge.completeActiveDay,
+        text: 'Complete',
+        leftIcon: FontAwesomeIcons.solidSquareCheck,
       ),
     );
   }
