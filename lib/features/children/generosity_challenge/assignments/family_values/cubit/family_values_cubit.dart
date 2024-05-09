e487827d9531590938a2481ea1be335fd28b9d58
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-05-09 17:00:15 +0200

    Implement day 2 + store values in userdata

--- lib/features/children/generosity_challenge/assignments/family_values/cubit/family_values_cubit.dart
+++ lib/features/children/generosity_challenge/assignments/family_values/cubit/family_values_cubit.dart
@@ -2,15 +2,16 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:givt_app/core/logging/logging_service.dart';
 import 'package:givt_app/features/children/generosity_challenge/assignments/family_values/models/family_value.dart';
-import 'package:givt_app/features/children/generosity_challenge/assignments/family_values/repositories/family_values_repository.dart';
+import 'package:givt_app/features/children/generosity_challenge/repositories/generosity_challenge_repository.dart';
+import 'package:givt_app/features/children/generosity_challenge_chat/chat_scripts/models/enums/chat_script_save_key.dart';
 
 part 'family_values_state.dart';
 
 class FamilyValuesCubit extends Cubit<FamilyValuesState> {
-  FamilyValuesCubit({required this.valuesRepository})
+  FamilyValuesCubit({required this.generosityChallengeRepository})
       : super(const FamilyValuesState(selectedValues: []));
 
-  final FamilyValuesRepository valuesRepository;
+  final GenerosityChallengeRepository generosityChallengeRepository;
 
   static const String familyValuesKey = 'family_values';
 
@@ -27,7 +28,33 @@ class FamilyValuesCubit extends Cubit<FamilyValuesState> {
 
   Future<void> rememberValues() async {
     try {
-      await valuesRepository.rememberValues(values: state.selectedValues);
+      // I guess it's ugly, but it does what it needs to do ¯\_(ツ)_/¯
+      
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue1Value,
+        state.selectedValues[0].displayText,
+      );
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue1Key,
+        state.selectedValues[0].area.name,
+      );
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue2Value,
+        state.selectedValues[1].displayText,
+      );
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue2Key,
+        state.selectedValues[1].area.name,
+      );
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue3Value,
+        state.selectedValues[2].displayText,
+      );
+      await generosityChallengeRepository.saveUserData(
+        ChatScriptSaveKey.familyValue3Key,
+        state.selectedValues[2].area.name,
+      );
+
     } on Exception catch (e) {
       await LoggingInfo.instance.error(
         e.toString(),
