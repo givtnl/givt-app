c667a77a6a554a8fbee5824f7e6437a396176f6e
TammiLion <sierrassets@tammidev.com>
2024-05-14 09:29:14 +0200

    Fix generosity repo crashing on loading from cache.

--- lib/features/children/generosity_challenge/repositories/generosity_challenge_repository.dart
+++ lib/features/children/generosity_challenge/repositories/generosity_challenge_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:givt_app/features/children/generosity_challenge/models/day.dart';
 import 'package:givt_app/features/children/generosity_challenge/utils/generosity_challenge_helper.dart';
 import 'package:givt_app/features/children/generosity_challenge_chat/chat_scripts/models/enums/chat_script_save_key.dart';
@@ -6,9 +7,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 mixin GenerosityChallengeRepository {
   Future<void> saveToCache(List<Day> days);
+
   Future<List<Day>> loadFromCache();
+
   Future<void> clearCache();
+
   Future<void> saveUserData(ChatScriptSaveKey key, String value);
+
   Map<String, dynamic> loadUserData();
 }
 
@@ -35,23 +40,35 @@ class GenerosityChallengeRepositoryImpl with GenerosityChallengeRepository {
 
   @override
   Future<List<Day>> loadFromCache() async {
-    final encodedString =
-        sharedPreferences.getString(_generosityChallengeDaysKey) ?? '';
-    if (encodedString.isNotEmpty) {
-      final result = (jsonDecode(encodedString) as List<dynamic>)
-          .map<Day>(
-            (day) => Day.fromMap(day as Map<String, dynamic>),
-          )
-          .toList();
-      return result;
-    } else {
-      final days = List<Day>.filled(
-        GenerosityChallengeHelper.generosityChallengeDays,
-        const Day.empty(),
-      );
-      await saveToCache(days);
-      return days;
+    try {
+      final encodedString =
+          sharedPreferences.getString(_generosityChallengeDaysKey) ?? '';
+      if (encodedString.isNotEmpty) {
+        final result = (jsonDecode(encodedString) as List<dynamic>)
+            .map<Day>(
+              (day) => Day.fromMap(day as Map<String, dynamic>),
+            )
+            .toList();
+        return result;
+      } else {
+        return await _saveAndReturnEmptyDays();
+      }
+    } catch (e, s) {
+      if (kDebugMode) {
+        print(e);
+        print(s);
+      }
+      return _saveAndReturnEmptyDays();
     }
+  }
+
+  Future<List<Day>> _saveAndReturnEmptyDays() async {
+    final days = List<Day>.filled(
+      GenerosityChallengeHelper.generosityChallengeDays,
+      const Day.empty(),
+    );
+    await saveToCache(days);
+    return days;
   }
 
   @override
