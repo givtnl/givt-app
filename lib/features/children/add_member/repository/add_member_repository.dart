a6d83a122c803781c76bc5af2e70ac4ddec6242a
TammiLion <sierrassets@tammidev.com>
2024-06-12 16:10:30 +0200

    Refresh profile selection when members are being added.

--- lib/features/children/add_member/repository/add_member_repository.dart
+++ lib/features/children/add_member/repository/add_member_repository.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:givt_app/core/network/api_service.dart';
 import 'package:givt_app/features/children/add_member/models/member.dart';
 
 mixin AddMemberRepository {
   Future<void> addMembers(List<Member> members);
+
+  Stream<void> memberAddedStream();
 }
 
 class AddMemberRepositoryImpl with AddMemberRepository {
   AddMemberRepositoryImpl(this.apiService);
+
   final APIService apiService;
+
+  final StreamController<void> _memberAddedStreamController =
+      StreamController<void>.broadcast();
 
   @override
   Future<void> addMembers(List<Member> members) async {
@@ -17,5 +25,9 @@ class AddMemberRepositoryImpl with AddMemberRepository {
     };
 
     await apiService.addMember(body);
+    _memberAddedStreamController.add(null);
   }
+
+  @override
+  Stream<void> memberAddedStream() => _memberAddedStreamController.stream;
 }
