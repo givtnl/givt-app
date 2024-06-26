d08be2043db8bc4f47e6519d1f5b0cd8877ccb74
TammiLion <sierrassets@tammidev.com>
2024-06-26 10:22:08 +0200

    Fix wrong theming for several family pages.

--- lib/features/children/overview/pages/edit_allowance_success_page.dart
+++ lib/features/children/overview/pages/edit_allowance_success_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:givt_app/features/children/overview/pages/models/edit_allowance_success_uimodel.dart';
 import 'package:givt_app/l10n/l10n.dart';
 import 'package:givt_app/shared/pages/common_success_page.dart';
+import 'package:givt_app/utils/app_theme.dart';
 
 class EditAllowanceSuccessPage extends StatelessWidget {
   const EditAllowanceSuccessPage({required this.uiModel, super.key,});
@@ -10,15 +11,18 @@ class EditAllowanceSuccessPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CommonSuccessPage(
-      buttonText: context.l10n.ready,
-      title: context.l10n.genericSuccessTitle,
-      text: uiModel.isMultipleChildren
-          ? 'Your children will receive ${uiModel.amountWithCurrencySymbol} each month. You can edit this amount any time.'
-          : context.l10n.monthlyAllowanceEditSuccessDescription(
-              uiModel.amountWithCurrencySymbol ?? '',
-            ),
-      onClickButton: uiModel.onClickButton,
+    return Theme(
+      data: AppTheme.lightTheme,
+      child: CommonSuccessPage(
+        buttonText: context.l10n.ready,
+        title: context.l10n.genericSuccessTitle,
+        text: uiModel.isMultipleChildren
+            ? 'Your children will receive ${uiModel.amountWithCurrencySymbol} each month. You can edit this amount any time.'
+            : context.l10n.monthlyAllowanceEditSuccessDescription(
+                uiModel.amountWithCurrencySymbol ?? '',
+              ),
+        onClickButton: uiModel.onClickButton,
+      ),
     );
   }
 }
