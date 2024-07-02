cd57591e501922641134c97a40aeaf73fc285522
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-07-02 15:57:28 +0200

    replace success page with profile overview

--- lib/features/registration/pages/registration_success_us.dart
+++ lib/features/registration/pages/registration_success_us.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:givt_app/app/routes/pages.dart';
 import 'package:givt_app/features/family/app/family_pages.dart';
 import 'package:givt_app/features/registration/widgets/registered_check_animation.dart';
 import 'package:givt_app/l10n/l10n.dart';
@@ -44,7 +43,9 @@ class RegistrationSuccessUs extends StatelessWidget {
                   const Spacer(),
                   CustomGreenElevatedButton(
                     title: context.l10n.setUpFamily,
-                    onPressed: () => context.pushNamed(FamilyPages.addMember.name),
+                    onPressed: () => context
+                      ..pushReplacementNamed(FamilyPages.addMember.name)
+                      ..pushNamed(FamilyPages.addMember.name),
                   ),
                   const SizedBox(height: 10),
                 ],
