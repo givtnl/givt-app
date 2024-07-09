ca7aae6bf4c05cbb2a50122a7a5b81682d8c4f93
TammiLion <sierrassets@tammidev.com>
2024-07-09 11:37:17 +0200

    Handle initial client generation when we don't have any internet connection.

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 mixin NetworkInfo {
   bool get isConnected;
+
+  Stream<bool> hasInternetConnectionStream();
 }
 
 class NetworkInfoImpl implements NetworkInfo {
@@ -9,16 +13,26 @@ class NetworkInfoImpl implements NetworkInfo {
     // First check if there is an internet connection
     connectionChecker.hasInternetAccess.then((value) {
       isConnected = value;
+      _hasInternetConnectionStream.add(value);
     });
 
     // Listen for changes in the internet connection status
     connectionChecker.onStatusChange.listen((status) {
       isConnected = status == InternetStatus.connected;
+      _hasInternetConnectionStream.add(isConnected);
     });
   }
+
+  // bool hasInternetConnection
+  final StreamController<bool> _hasInternetConnectionStream =
+      StreamController<bool>.broadcast();
 
   @override
   bool isConnected = true;
 
   final InternetConnection connectionChecker;
+
+  @override
+  Stream<bool> hasInternetConnectionStream() =>
+      _hasInternetConnectionStream.stream;
 }
