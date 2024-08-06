4797fc4113ae0b17d06f620c9bdc8344a1184f4a
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-08-06 10:14:27 +0300

    rabbit improve

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -84,19 +84,8 @@ AgMBAAE=
 
   static String formatPhoneNrUs(String input) {
     final numericOnly = input.replaceAll(RegExp(r'[^\d]'), '');
-    var formatted = '';
-    if (numericOnly.length == 10) {
-      final chunkSize = [3, 3, 4];
-      var startIndex = 0;
-
-      final chunks = chunkSize.map((size) {
-        final chunk = numericOnly.substring(startIndex, startIndex + size);
-        startIndex += size;
-        return chunk;
-      });
-      formatted = chunks.join('-');
-    }
-    return formatted;
+    if (numericOnly.length != 10) return '';
+    return '${numericOnly.substring(0, 3)}-${numericOnly.substring(3, 6)}-${numericOnly.substring(6)}';
   }
 
   static String getCurrencySymbol({required String countryCode}) {
