41790c13dff26dff024823c80e1bea5e41fe13ce
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-08-28 11:10:09 +0200

    route fix

--- lib/features/family/app/family_routes.dart
+++ lib/features/family/app/family_routes.dart
@@ -76,7 +76,7 @@ import 'package:givt_app/features/family/features/recommendation/organisations/s
 import 'package:givt_app/features/family/features/recommendation/start_recommendation/start_recommendation_screen.dart';
 import 'package:givt_app/features/family/features/recommendation/tags/cubit/tags_cubit.dart';
 import 'package:givt_app/features/family/features/recommendation/tags/screens/location_selection_screen.dart';
-import 'package:givt_app/features/family/features/reflect/reflect_intro_screen.dart';
+import 'package:givt_app/features/family/features/reflect/presentation/pages/reflect_intro_screen.dart';
 import 'package:givt_app/features/family/features/scan_nfc/nfc_scan_screen.dart';
 import 'package:givt_app/features/give/bloc/give/give_bloc.dart';
 import 'package:givt_app/features/give/models/organisation.dart';
