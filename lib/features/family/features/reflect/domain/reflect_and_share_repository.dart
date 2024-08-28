07396b6974de053de321841052874b7402ae59be
TammiLion <sierrassets@tammidev.com>
2024-08-28 11:59:41 +0200

    More logic.

--- lib/features/family/features/reflect/domain/reflect_and_share_repository.dart
+++ lib/features/family/features/reflect/domain/reflect_and_share_repository.dart
@@ -2,11 +2,15 @@ import 'dart:math';
 
 import 'package:givt_app/features/family/features/profiles/repository/profiles_repository.dart';
 import 'package:givt_app/features/family/features/reflect/domain/models/game_profile.dart';
+import 'package:givt_app/features/family/features/reflect/domain/models/roles.dart';
 
 class ReflectAndShareRepository {
   ReflectAndShareRepository(this._profilesRepository);
 
   final ProfilesRepository _profilesRepository;
+
+  // list to keep track if everyone has been the superhero yet
+  List<int> superheroes = [];
 
   List<GameProfile>? allProfiles;
   List<GameProfile> selectedProfiles = [];
@@ -39,19 +43,66 @@ class ReflectAndShareRepository {
 
   List<GameProfile> randomlyAssignRoles() {
     final rng = Random();
-    final superhero = rng.nextInt(allProfiles!.length);
-    if(superhero == allProfiles!.length -1) {
-
+    var sidekickIndex;
+    final superheroIndex = rng.nextInt(allProfiles!.length);
+    if (_isLastIndex(superheroIndex)) {
+      sidekickIndex = 0;
+    } else {
+      sidekickIndex = superheroIndex + 1;
     }
-    //TODO
+
+    final questions = getQuestions();
+    selectedProfiles.asMap().forEach((index, profile) {
+      if (index == superheroIndex) {
+        selectedProfiles[index] =
+            profile.copyWith(role: Role.superhero(secretWord: getSecretWord()));
+      } else if (index == sidekickIndex) {
+        selectedProfiles[index] = profile.copyWith(role: const Role.sidekick());
+      } else {
+        selectedProfiles[index] = profile.copyWith(
+            role: Role.reporter(questions: [questions[index]]));
+      }
+    });
+    final amountReporters = selectedProfiles.length - 2;
     return selectedProfiles;
   }
+
+  String getSecretWord() {
+    return "banana";
+  }
+
+  String rerollSecretWord() {
+    final GameProfile? profile = null;
+    final index = selectedProfiles.indexWhere((profile) {
+      if (profile.role is SuperHero) {
+        profile = profile;
+        return true;
+      }
+      return false;
+    });
+
+    selectedProfiles[index] =
+        profile!.copyWith(role: Role.superhero(secretWord: "macarena"));
+    return "macarena";
+  }
+
+  bool _isLastIndex(int superheroIndex) =>
+      superheroIndex == allProfiles!.length - 1;
 
   List<GameProfile> getSelectedProfiles() {
     return [];
   }
 
-  Future<List<String>> getQuestions() async {
-    return ["what?", "how?", "when?"];
+  List<String> getQuestions() {
+    return [
+      "what is Gamora?",
+      "how is Gamora?",
+      "when is Gamora?",
+      "where is Gamora?",
+      "who is Gamora?",
+      "why is Gamora?",
+      "what is a non-interesting fact about yourself?",
+      "did you dance today?"
+    ];
   }
 }
