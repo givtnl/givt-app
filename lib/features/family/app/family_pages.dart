397bfa9d9a67f4405368b0cbff72f1ddbc975821
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-09-02 14:42:52 +0200

    remove more code

--- lib/features/family/app/family_pages.dart
+++ lib/features/family/app/family_pages.dart
@@ -79,12 +79,6 @@ enum FamilyPages {
     path: 'choose-amount-slider-goal',
     name: 'CHOOSE_AMOUNT_SLIDER_GOAL',
   ),
-
-  //generosity challenge
-  generosityChallengeRedirect(
-    path: '/generosity-challenge-redirect',
-    name: 'GENEROSITY-CHALLENGE-REDIRECT',
-  ),
   generosityChallenge(
     path: '/generosity-challenge',
     name: 'GENEROSITY-CHALLENGE',
