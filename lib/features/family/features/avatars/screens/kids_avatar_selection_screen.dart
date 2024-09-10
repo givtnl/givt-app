5cee8c09cd27d6b6ab5bece500173bd107bb3b2a
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-09-11 00:25:06 +0200

    Refactor avatar selection screens with components

--- lib/features/family/features/avatars/screens/kids_avatar_selection_screen.dart
+++ lib/features/family/features/avatars/screens/kids_avatar_selection_screen.dart
@@ -7,10 +7,12 @@ import 'package:givt_app/features/family/features/avatars/cubit/avatars_cubit.da
 import 'package:givt_app/features/family/features/avatars/widgets/avatar_item.dart';
 import 'package:givt_app/features/family/features/edit_profile/cubit/edit_profile_cubit.dart';
 import 'package:givt_app/features/family/shared/design/components/components.dart';
+import 'package:givt_app/features/family/shared/widgets/buttons/givt_back_button_flat.dart';
 import 'package:givt_app/features/family/shared/widgets/dialogs/reward_banner_dialog.dart';
 import 'package:givt_app/features/family/shared/widgets/loading/custom_progress_indicator.dart';
 import 'package:givt_app/features/family/shared/widgets/texts/shared_texts.dart';
 import 'package:givt_app/shared/models/analytics_event.dart';
+import 'package:givt_app/shared/widgets/fun_scaffold.dart';
 import 'package:givt_app/utils/analytics_helper.dart';
 import 'package:givt_app/utils/snack_bar_helper.dart';
 import 'package:go_router/go_router.dart';
@@ -57,28 +59,17 @@ class KidsAvatarSelectionScreen extends StatelessWidget {
           }
         },
         builder: (context, avatarsState) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const TitleLargeText(
-                'Choose your avatar',
-              ),
-              automaticallyImplyLeading: false,
+          return FunScaffold(
+            appBar: FunTopAppBar(
+              title: 'Choose your avatar',
               leading: editProfileState.status != EditChildProfileStatus.editing
-                  ? IconButton(
-                      icon: const FaIcon(FontAwesomeIcons.arrowLeft),
-                      onPressed: () {
-                        SystemSound.play(SystemSoundType.click);
-                        context.pop();
-                      },
-                    )
+                  ? const GivtBackButtonFlat()
                   : null,
             ),
-            body: SafeArea(
-              child: _getContent(
-                context: context,
-                avatarsState: avatarsState,
-                editProfileState: editProfileState,
-              ),
+            body: _getContent(
+              context: context,
+              avatarsState: avatarsState,
+              editProfileState: editProfileState,
             ),
           );
         },
@@ -100,33 +91,27 @@ Widget _getContent({
   if (avatarsState.status == AvatarsStatus.loaded) {
     return CustomScrollView(
       slivers: [
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24,
-            vertical: 24,
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return AvatarItem(
+                onSelectProfilePicture: (profilePicture) {
+                  context
+                      .read<EditChildProfileCubit>()
+                      .selectProfilePicture(profilePicture);
+                },
+                filename: avatarsState.avatars[index].fileName,
+                url: avatarsState.avatars[index].pictureURL,
+                isSelected: avatarsState.avatars[index].fileName ==
+                    editProfileState.selectedProfilePicture,
+              );
+            },
+            childCount: avatarsState.avatars.length,
           ),
-          sliver: SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return AvatarItem(
-                  onSelectProfilePicture: (profilePicture) {
-                    context
-                        .read<EditChildProfileCubit>()
-                        .selectProfilePicture(profilePicture);
-                  },
-                  filename: avatarsState.avatars[index].fileName,
-                  url: avatarsState.avatars[index].pictureURL,
-                  isSelected: avatarsState.avatars[index].fileName ==
-                      editProfileState.selectedProfilePicture,
-                );
-              },
-              childCount: avatarsState.avatars.length,
-            ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-            ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
           ),
         ),
         SliverFillRemaining(
@@ -135,21 +120,18 @@ Widget _getContent({
             mainAxisSize: MainAxisSize.min,
             children: [
               const Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 24, left: 24, right: 24),
-                child: FunButton(
-                  text: 'Save',
-                  isDisabled: editProfileState.isSameProfilePicture,
-                  onTap: () {
-                    context.read<EditChildProfileCubit>().editProfile();
+              FunButton(
+                text: 'Save',
+                isDisabled: editProfileState.isSameProfilePicture,
+                onTap: () {
+                  context.read<EditChildProfileCubit>().editProfile();
+                },
+                analyticsEvent: AnalyticsEvent(
+                  AmplitudeEvents.saveAvatarClicked,
+                  parameters: {
+                    AnalyticsHelper.avatarImageKey:
+                        editProfileState.selectedProfilePicture,
                   },
-                  analyticsEvent: AnalyticsEvent(
-                    AmplitudeEvents.saveAvatarClicked,
-                    parameters: {
-                      AnalyticsHelper.avatarImageKey:
-                          editProfileState.selectedProfilePicture,
-                    },
-                  ),
                 ),
               ),
             ],
