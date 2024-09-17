581fadec4877a4a14168b65b07253aa8e4183f76
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-09-17 20:32:18 +0200

    fix last john

--- lib/features/registration/pages/registration_success_us.dart
+++ lib/features/registration/pages/registration_success_us.dart
@@ -8,7 +8,6 @@ import 'package:givt_app/features/registration/widgets/registered_check_animatio
 import 'package:givt_app/l10n/l10n.dart';
 import 'package:givt_app/shared/models/analytics_event.dart';
 import 'package:givt_app/shared/widgets/fun_scaffold.dart';
-import 'package:givt_app/utils/app_theme.dart';
 import 'package:go_router/go_router.dart';
 
 class RegistrationSuccessUs extends StatelessWidget {
@@ -18,38 +17,32 @@ class RegistrationSuccessUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      onPopInvoked: (didPop) =>
-          context.goNamed(FamilyPages.profileSelection.name),
-      child: Theme(
-        data: AppTheme.lightTheme,
-        child: FunScaffold(
-          appBar: FunTopAppBar.primary99(
-            title: 'Registration complete',
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                const Spacer(),
-                const RegisteredCheckAnimation(),
-                const Spacer(),
-                FunButton(
-                  text: context.l10n.setUpFamily,
-                  onTap: () {
-                    context.pushReplacementNamed(
-                      FamilyPages.profileSelection.name,
-                    );
-                  },
-                  analyticsEvent: AnalyticsEvent(
-                    AmplitudeEvents.registrationSuccesButtonClicked,
-                    parameters: {
-                      'id': context.read<AuthCubit>().state.user.guid,
-                    },
-                  ),
-                ),
-              ],
+    return FunScaffold(
+      appBar: FunTopAppBar.primary99(
+        title: 'Registration complete',
+      ),
+      canPop: false,
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            const RegisteredCheckAnimation(),
+            const Spacer(),
+            FunButton(
+              text: context.l10n.setUpFamily,
+              onTap: () {
+                context.pushReplacementNamed(
+                  FamilyPages.profileSelection.name,
+                );
+              },
+              analyticsEvent: AnalyticsEvent(
+                AmplitudeEvents.registrationSuccesButtonClicked,
+                parameters: {
+                  'id': context.read<AuthCubit>().state.user.guid,
+                },
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
