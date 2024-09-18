1887c091d3d3443876b622d670aa63995524d38b
TammiLion <sierrassets@tammidev.com>
2024-09-18 22:42:53 +0200

    Reapply "Create grateful avatar bar."
    This reverts commit 956ef3688e216d0591bf7724b6eb11fab3a6ff9f.

--- lib/features/family/shared/design/components/navigation/fun_top_app_bar.dart
+++ lib/features/family/shared/design/components/navigation/fun_top_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/features/family/shared/widgets/texts/shared_texts.dart';
 import 'package:givt_app/features/family/utils/family_app_theme.dart';
 
