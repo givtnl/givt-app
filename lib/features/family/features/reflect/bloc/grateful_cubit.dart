16866f68f905a93d92add041824073182eed489a
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-09-19 19:49:32 +0200

    adjusting error logic

--- lib/features/family/features/reflect/bloc/grateful_cubit.dart
+++ lib/features/family/features/reflect/bloc/grateful_cubit.dart
@@ -27,7 +27,7 @@ class GratefulCubit extends CommonCubit<GratefulUIModel, GratefulCustom> {
     _initProfiles();
 
     await _gratefulRecommendationsRepository
-        .getGratefulRecommendationsForMultipleProfiles(_profiles);
+        .fetchGratefulRecommendationsForMultipleProfiles(_profiles);
 
     _currentRecommendations = await _gratefulRecommendationsRepository
         .getGratefulRecommendations(_getCurrentProfile());
--- lib/features/family/features/reflect/domain/grateful_recommendations_repository_impl.dart
+++ lib/features/family/features/reflect/domain/grateful_recommendations_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:givt_app/core/logging/logging_service.dart';
 import 'package:givt_app/features/family/features/recommendation/organisations/models/organisation.dart';
 import 'package:givt_app/features/family/features/reflect/domain/grateful_recommendations_repository.dart';
 import 'package:givt_app/features/family/features/reflect/domain/models/game_profile.dart';
@@ -13,37 +14,43 @@ class GratefulRecommendationsRepositoryImpl
   final Map<GameProfile, List<Organisation>> _gratefulRecommendations = {};
 
   @override
-  Future<Map<GameProfile, List<Organisation>>>
-      getGratefulRecommendationsForMultipleProfiles(
+  Future<void> fetchGratefulRecommendationsForMultipleProfiles(
     List<GameProfile> profiles,
   ) async {
-    final results = await Future.wait(
-      profiles.map((profile) async {
-        final interests = profile.gratitude?.tags;
-        final response = await _familyApiService.getRecommendedOrganisations(
-          {
-            'pageSize': 10,
-            'tags': interests?.map((tag) => tag.key).toList(),
-          },
-        );
+    try {
+      final results = await Future.wait(
+        profiles.map((profile) async {
+          final interests = profile.gratitude?.tags;
+          final response = await _familyApiService.getRecommendedOrganisations(
+            {
+              'pageSize': 10,
+              'tags': interests?.map((tag) => tag.key).toList(),
+            },
+          );
 
-        final orgsList = response
-            .map((org) => Organisation.fromMap(org as Map<String, dynamic>));
-        return MapEntry(profile, orgsList.toList());
-      }),
-    );
+          final orgsList = response
+              .map((org) => Organisation.fromMap(org as Map<String, dynamic>));
+          return MapEntry(profile, orgsList.toList());
+        }),
+      );
 
-    _gratefulRecommendations.addEntries(results);
-    return _gratefulRecommendations;
+      _gratefulRecommendations.addEntries(results);
+    } catch (e, s) {
+      LoggingInfo.instance.error(
+        e.toString(),
+        methodName: s.toString(),
+      );
+    }
   }
 
   @override
   Future<List<Organisation>> getGratefulRecommendations(
     GameProfile profile,
   ) async {
-    return _gratefulRecommendations[profile] ??
-        (await getGratefulRecommendationsForMultipleProfiles([profile]))
-            .values
-            .first;
+    if (!_gratefulRecommendations.containsKey(profile)) {
+      await fetchGratefulRecommendationsForMultipleProfiles([profile]);
+    }
+
+    return _gratefulRecommendations[profile] ?? [];
   }
 }
