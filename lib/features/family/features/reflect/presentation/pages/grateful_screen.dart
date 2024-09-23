31943aac7baf35bfa3b872e522b8a52ae1272f0f
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-09-23 11:53:35 +0200

    reuse component

--- lib/features/family/features/reflect/presentation/pages/grateful_screen.dart
+++ lib/features/family/features/reflect/presentation/pages/grateful_screen.dart
@@ -8,9 +8,10 @@ import 'package:givt_app/features/family/features/profiles/cubit/profiles_cubit.
 import 'package:givt_app/features/family/features/reflect/bloc/grateful_cubit.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/models/grateful_custom.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/pages/summary_screen.dart';
-import 'package:givt_app/features/family/features/reflect/presentation/widgets/finish_reflection_buttton.dart';
+import 'package:givt_app/features/family/features/reflect/presentation/widgets/finish_reflection_dialog.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/widgets/grateful_avatar_bar.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/widgets/grateful_loading.dart';
+import 'package:givt_app/features/family/features/reflect/presentation/widgets/leave_game_button.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/widgets/recommendations_widget.dart';
 import 'package:givt_app/features/family/shared/design/components/components.dart';
 import 'package:givt_app/shared/widgets/base/base_state_consumer.dart';
@@ -48,10 +49,12 @@ class _GratefulScreenState extends State<GratefulScreen> {
       onData: (context, uiModel) {
         return FunScaffold(
           withSafeArea: false,
-          appBar: const FunTopAppBar(
+          appBar: FunTopAppBar(
             title: "You're grateful for",
             actions: [
-              FinishReflectingButton(),
+              LeaveGameButton(
+                onPressed: () => FinishReflectionDialog().show(context),
+              ),
             ],
           ),
           body: Column(
--- lib/features/family/features/reflect/presentation/widgets/leave_game_button.dart
+++ lib/features/family/features/reflect/presentation/widgets/leave_game_button.dart
@@ -3,15 +3,19 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/widgets/leave_game_dialog.dart';
 
 class LeaveGameButton extends StatelessWidget {
-  const LeaveGameButton({super.key});
-
+  const LeaveGameButton({
+    super.key,
+    this.onPressed,
+  });
+  final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
     return IconButton(
       icon: const FaIcon(FontAwesomeIcons.xmark),
-      onPressed: () {
-        const LeaveGameDialog().show(context);
-      },
+      onPressed: onPressed ??
+          () {
+            const LeaveGameDialog().show(context);
+          },
     );
   }
 }
