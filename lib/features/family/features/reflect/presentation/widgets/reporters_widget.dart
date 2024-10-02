b831b0e85cad9a07517c94ddf267dfca280b2660
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-10-02 15:34:22 +0200

    simplify reporters widget

--- lib/features/family/features/reflect/presentation/widgets/reporters_widget.dart
+++ lib/features/family/features/reflect/presentation/widgets/reporters_widget.dart
@@ -3,94 +3,64 @@ import 'package:givt_app/features/family/features/reflect/domain/models/game_pro
 import 'package:givt_app/features/family/features/reflect/presentation/widgets/game_profile_item.dart';
 
 class ReportersWidget extends StatelessWidget {
-  const ReportersWidget(
-      {required this.reporters,
-      this.circleSize = 100,
-      this.displayName = true,
-      super.key});
+  const ReportersWidget({
+    required this.reporters,
+    this.circleSize = 100,
+    this.displayName = true,
+    super.key,
+  });
+
   final List<GameProfile> reporters;
   final double circleSize;
   final bool displayName;
+
   @override
   Widget build(BuildContext context) {
-    switch (reporters.length) {
-      case 1:
-        return GameProfileItem(
-          profile: reporters.first,
-          displayRole: false,
-          displayName: displayName,
-          size: circleSize,
-        );
-      case 2:
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: SizedBox(
-            width: double.infinity,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  left: 1,
-                  child: SizedBox(
-                    height: circleSize - 28,
-                    width: circleSize - 28,
-                  ),
-                ),
-                Positioned(
-                  right: 8,
-                  child: GameProfileItem(
-                    profile: reporters[1],
-                    size: circleSize - 20,
-                    displayRole: false,
-                    displayName: displayName,
-                  ),
-                ),
-                GameProfileItem(
-                  profile: reporters.first,
-                  displayRole: false,
-                  displayName: displayName,
-                  size: circleSize,
-                ),
-              ],
-            ),
-          ),
-        );
-      case >= 3:
-        final reportersShort = reporters.take(3).toList();
-        return SizedBox(
-          width: double.infinity,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              ...reportersShort.reversed.map((reporter) {
-                return Positioned(
-                  right: reportersShort.indexOf(reporter) == 1 ? 28 : null,
-                  left: reportersShort.indexOf(reporter) == 2 ? 28 : null,
-                  child: reportersShort.indexOf(reporter) == 0
-                      ? GameProfileItem(
-                          profile: reporter,
-                          size: circleSize,
-                          displayRole: false,
-                          displayName: displayName,
-                        )
-                      : GameProfileItem(
-                          profile: reporter,
-                          size: circleSize - 20,
-                          displayRole: false,
-                          displayName: displayName,
-                        ),
-                );
-              }),
-            ],
-          ),
-        );
-      default:
-        return GameProfileItem(
-          profile: reporters.first,
-          displayName: displayName,
-          displayRole: false,
-          size: circleSize,
-        );
+    if (reporters.isEmpty) {
+      return const SizedBox.shrink();
     }
+
+    return Padding(
+      padding: reporters.length == 2
+          ? const EdgeInsets.symmetric(horizontal: 20)
+          : EdgeInsets.zero,
+      child: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: _buildProfiles().reversed.toList(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildProfiles() {
+    final profilesToShow = reporters.take(3).toList();
+    return List.generate(profilesToShow.length, (index) {
+      var size = circleSize;
+      double? left;
+      double? right;
+
+      // Adjust position and size based on the number of reporters
+      if (reporters.length == 2) {
+        size -= (index == 1 ? 20 : 0); // Smaller for second profile
+        right = (index == 1 ? 8 : null);
+      } else if (reporters.length >= 3) {
+        size -= (index > 0 ? 20 : 0);
+        left = (index == 2 ? 28 : null);
+        right = (index == 1 ? 28 : null);
+      }
+
+      return Positioned(
+        left: left,
+        right: right,
+        child: GameProfileItem(
+          profile: profilesToShow[index],
+          size: size,
+          displayRole: false,
+          displayName: displayName,
+        ),
+      );
+    });
   }
 }
