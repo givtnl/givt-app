23ce0c91a57aba2e3b0d24392b7efe4cbe09eea9
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-10-07 11:53:49 +0200

    remove use cases

--- lib/features/family/features/home_screen/cubit/family_home_screen_cubit.dart
+++ lib/features/family/features/home_screen/cubit/family_home_screen_cubit.dart
@@ -1,8 +1,4 @@
-import 'package:givt_app/features/children/add_member/models/member.dart';
 import 'package:givt_app/features/family/features/home_screen/presentation/models/family_home_screen.uimodel.dart';
-import 'package:givt_app/features/family/features/home_screen/usecases/family_setup_usecase.dart';
-import 'package:givt_app/features/family/features/home_screen/usecases/preferred_church_usecase.dart';
-import 'package:givt_app/features/family/features/home_screen/usecases/registration_use_case.dart';
 import 'package:givt_app/features/family/features/profiles/models/profile.dart';
 import 'package:givt_app/features/family/features/profiles/repository/profiles_repository.dart';
 import 'package:givt_app/features/family/features/reflect/presentation/models/grateful_avatar_uimodel.dart';
@@ -12,8 +8,7 @@ import 'package:givt_app/shared/bloc/base_state.dart';
 import 'package:givt_app/shared/bloc/common_cubit.dart';
 
 class FamilyHomeScreenCubit
-    extends CommonCubit<FamilyHomeScreenUIModel, dynamic>
-    with PreferredChurchUseCase, RegistrationUseCase, FamilySetupUseCase {
+    extends CommonCubit<FamilyHomeScreenUIModel, dynamic> {
   FamilyHomeScreenCubit(
     this._profilesRepository,
     this._impactGroupsRepository,
