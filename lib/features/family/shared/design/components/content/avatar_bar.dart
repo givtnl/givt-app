94d4f34bd03d1450c23798da28bd86410f555f1b
TammiLion <sierrassets@tammidev.com>
2024-10-21 11:49:39 +0200

    Center avatar bar. Reduce dialog overlay time.

--- lib/features/family/shared/design/components/content/avatar_bar.dart
+++ lib/features/family/shared/design/components/content/avatar_bar.dart
@@ -21,22 +21,28 @@ class AvatarBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.sizeOf(context).width,
       color: backgroundColor,
-      padding: const EdgeInsets.only(left: 24, top: 8, bottom: 16),
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(top: 8, bottom: 16),
       child: SizedBox(
         height: 92,
         child: ListView.builder(
+          shrinkWrap: true,
           scrollDirection: Axis.horizontal,
           itemCount: uiModel.avatarUIModels.length,
           itemBuilder: (context, index) {
             final avatarUIModel = uiModel.avatarUIModels[index];
             return Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: AvatarWidget(
-                uiModel: avatarUIModel,
-                onTap: () => onAvatarTapped(index),
-                circleSize: circleSize,
-                textColor: textColor,
+              child: Padding(
+                padding: EdgeInsets.only(left: index == 0 ? 24 : 0),
+                child: AvatarWidget(
+                  uiModel: avatarUIModel,
+                  onTap: () => onAvatarTapped(index),
+                  circleSize: circleSize,
+                  textColor: textColor,
+                ),
               ),
             );
           },
