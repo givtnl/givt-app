ea32cb4a057c9d0e77b8aa962855a3c1203c2aa5
TammiLion <sierrassets@tammidev.com>
2024-10-23 13:33:34 +0200

    Fix changes.

--- lib/features/family/features/auth/bloc/family_auth_cubit.dart
+++ lib/features/family/features/auth/bloc/family_auth_cubit.dart
@@ -4,28 +4,24 @@ import 'package:givt_app/features/family/features/auth/presentation/models/famil
 import 'package:givt_app/shared/models/models.dart';
 
 class FamilyAuthCubit extends Cubit<FamilyAuthState> {
-  FamilyAuthCubit(this._authRepository) : super(const FamilyAuthState.unauthenticated()) {
+  FamilyAuthCubit(this._authRepository)
+      : super(const FamilyAuthState.unauthenticated()) {
     _init();
   }
 
   final FamilyAuthRepository _authRepository;
 
   Future<void> _init() async {
-    _authRepository.authenticatedUserStream().listen(_handleAuthenticationUpdate);
+    _authRepository
+        .authenticatedUserStream()
+        .listen(_handleAuthenticationUpdate);
   }
 
-  void _handleAuthenticationUpdate(bool hasSession) {
-    if(hasSession) {
-     _authRepository.isAuthenticated()
+  void _handleAuthenticationUpdate(UserExt? userExt) {
+    if (userExt != null) {
+      emit(FamilyAuthState.authenticated(userExt));
     } else {
       emit(const FamilyAuthState.unauthenticated());
     }
   }
-
-  void _handleUserUpdate(UserExt? userExt) {
-    if (userExt != null) {
-      emit(FamilyAuthState.authenticated(userExt));
-    }
-  }
-
 }
