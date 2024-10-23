77811ce7d20edcad01ef870ec55440dd4e5e1a05
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-10-23 14:44:48 +0200

    pop on connection live

--- lib/shared/dialogs/internet_connection_lost_dialog.dart
+++ lib/shared/dialogs/internet_connection_lost_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:givt_app/features/internet_connection/internet_connection_cubit.dart';
 import 'package:givt_app/l10n/l10n.dart';
 import 'package:givt_app/shared/dialogs/warning_dialog.dart';
 import 'package:go_router/go_router.dart';
@@ -7,11 +9,19 @@ class InternetConnectionLostDialog {
   static void show(BuildContext context) {
     showDialog<void>(
       context: context,
-      builder: (_) => WarningDialog(
-        title: context.l10n.noInternetConnectionTitle,
-        content:
-            "Oops! It looks like you're not connected to the internet. Some features might not work as expected. Please come back when you have a connection.",
-        onConfirm: () => context.pop(),
+      builder: (_) =>
+          BlocListener<InternetConnectionCubit, InternetConnectionState>(
+        listener: (context, state) {
+          if (state is InternetConnectionLive) {
+            context.pop();
+          }
+        },
+        child: WarningDialog(
+          title: context.l10n.noInternetConnectionTitle,
+          content:
+              "Oops! It looks like you're not connected to the internet. Some features might not work as expected. Please come back when you have a connection.",
+          onConfirm: () => context.pop(),
+        ),
       ),
     );
   }
