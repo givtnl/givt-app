4a8298915d0ad9c6fda32c905e901410f2f57edc
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-10-23 13:47:24 +0200

    US edit info page should call new reset pass change

--- lib/features/family/features/account/presentation/pages/us_personal_info_edit_page.dart
+++ lib/features/family/features/account/presentation/pages/us_personal_info_edit_page.dart
@@ -16,6 +16,7 @@ import 'package:givt_app/features/auth/cubit/auth_cubit.dart';
 import 'package:givt_app/features/auth/pages/change_password_page.dart';
 import 'package:givt_app/features/family/app/family_pages.dart';
 import 'package:givt_app/features/family/features/auth/helpers/logout_helper.dart';
+import 'package:givt_app/features/family/features/reset_password/presentation/pages/reset_password_screen.dart';
 import 'package:givt_app/features/family/shared/design/components/components.dart';
 import 'package:givt_app/features/family/utils/family_app_theme.dart';
 import 'package:givt_app/features/registration/cubit/stripe_cubit.dart';
@@ -183,12 +184,8 @@ class USPersonalInfoEditPage extends StatelessWidget {
                   FontAwesomeIcons.lock,
                 ),
                 value: locals.changePassword,
-                onTap: () => _showModalBottomSheet(
-                  context,
-                  bottomSheet: ChangePasswordPage(
-                    email: user.email,
-                  ),
-                ),
+                onTap: () =>
+                    ResetPasswordScreen(initialEmail: user.email).show(context),
               ),
               FutureBuilder(
                 initialData: false,
