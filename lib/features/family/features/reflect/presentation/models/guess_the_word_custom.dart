072001fc08fd3b1d7baa51a5381dcacf0c5f9db4
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-10-31 13:23:07 +0100

    Update Guess the secret word screen

--- lib/features/family/features/reflect/presentation/models/guess_the_word_custom.dart
+++ lib/features/family/features/reflect/presentation/models/guess_the_word_custom.dart
@@ -1,11 +1,14 @@
-
-
 sealed class GuessTheWordCustom {
   const GuessTheWordCustom();
 
   const factory GuessTheWordCustom.showConfetti() = ShowConfetti;
+  const factory GuessTheWordCustom.redirectToSummary() = RedirectToSummary;
 }
 
 class ShowConfetti extends GuessTheWordCustom {
   const ShowConfetti();
+}
+
+class RedirectToSummary extends GuessTheWordCustom {
+  const RedirectToSummary();
 }
