5bfd9c9028539f2c0a5b303e77d9757915aeafea
TammiLion <sierrassets@tammidev.com>
2024-11-21 09:37:05 +0100

    Add barebones.

---
+++ integration_test/counter_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:givt_app/app/app.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+            (tester) async {
+          // Load app widget.
+          await tester.pumpWidget(const App());
+
+          // Verify the counter starts at 0.
+          expect(find.text('0'), findsOneWidget);
+
+          // Finds the floating action button to tap on.
+          final fab = find.byKey(const ValueKey('increment'));
+
+          // Emulate a tap on the floating action button.
+          await tester.tap(fab);
+
+          // Trigger a frame.
+          await tester.pumpAndSettle();
+
+          // Verify the counter increments by 1.
+          expect(find.text('1'), findsOneWidget);
+        });
+  });
+}
