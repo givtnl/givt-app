aaf01072e843db6cb8b163f947dd3d94b1e81e35
TammiLion <sierrassets@tammidev.com>
2024-11-29 16:13:43 +0100

    Remove fake data.

--- lib/features/family/features/gratitude-summary/bloc/parent_summary_cubit.dart
+++ lib/features/family/features/gratitude-summary/bloc/parent_summary_cubit.dart
@@ -26,79 +26,9 @@ class ParentSummaryCubit extends CommonCubit<ParentSummaryUIModel?, dynamic> {
       emitLoading();
       final summary = await _summaryRepository.fetchLatestGameSummary();
       _summary = summary;
-    } catch (e,s) {
+    } catch (e, s) {
       LoggingInfo.instance
           .error('Failed to fetch summary', methodName: 'fetchSummary');
-      _summary = ParentSummaryItem(
-        date: DateTime.now(),
-        audio: 'https://download.samplelib.com/mp3/sample-3s.mp3',
-        conversations: [
-          Conversation(
-              sentence:
-                  "Grateful for nature, I donated \$5 to Make-A-Wish to help make dreams come true.",
-              profile: Profile.fromMap(jsonDecode('''
-                {
-                  "id": "ea04efcc-f9c4-433d-90c3-8df1d14a9625",
-                  "firstName": "Debby",
-                  "lastName": null,
-                  "dateOfBirth": "2019-07-01T00:00:00",
-                  "givingAllowance": null,
-                  "bedTime": "18:00:00",
-                  "windDownTime": 30,
-                  "type": "Child",
-                  "picture": {
-                    "fileName": "Hero1.svg",
-                    "pictureURL": "https://givtstoragedebug.blob.core.windows.net/public/cdn/avatars/Hero7.svg"
-                  },
-                  "wallet": {
-                    "balance": 0.0,
-                    "total": 0.0,
-                    "pending": 0,
-                    "currency": "USD",
-                    "pendingAllowance": false,
-                    "givingAllowance": {
-                      "amount": 0,
-                      "nextGivingAllowance": "0001-01-01T00:00:00+00:00",
-                      "frequency": "None"
-                    }
-                  },
-                  "latestDonation": null
-                }
-              ''') as Map<String, dynamic>)),
-          Conversation(
-              sentence:
-              "Grateful for our home, I’m packing a care bag to help someone in need feel cared for.",
-              profile: Profile.fromMap(jsonDecode('''
-                {
-                  "id": "ea04efcc-f9c4-433d-90c3-8df1d14a9625",
-                  "firstName": "James",
-                  "lastName": null,
-                  "dateOfBirth": "2019-07-01T00:00:00",
-                  "givingAllowance": null,
-                  "bedTime": "18:00:00",
-                  "windDownTime": 30,
-                  "type": "Child",
-                  "picture": {
-                    "fileName": "Hero1.svg",
-                    "pictureURL": "https://givtstoragedebug.blob.core.windows.net/public/cdn/avatars/Hero4.svg"
-                  },
-                  "wallet": {
-                    "balance": 0.0,
-                    "total": 0.0,
-                    "pending": 0,
-                    "currency": "USD",
-                    "pendingAllowance": false,
-                    "givingAllowance": {
-                      "amount": 0,
-                      "nextGivingAllowance": "0001-01-01T00:00:00+00:00",
-                      "frequency": "None"
-                    }
-                  },
-                  "latestDonation": null
-                }
-              ''') as Map<String, dynamic>)),
-        ],
-      );
     }
     _emitData();
   }
