d2b36274f94999a8560f1cf0de1221be72ebab4c
TammiLion <sierrassets@tammidev.com>
2024-12-06 14:10:02 +0100

    Working on testing out patrol.

--- integration_test/eu_login_test.dart
+++ integration_test/eu_login_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:givt_app/app/app.dart';
+import 'package:patrol/patrol.dart';
 
 import 'features/auth/pages/login_test_page.dart';
 import 'features/email_signup/presentation/pages/email_signup_test_page.dart';
@@ -11,7 +12,9 @@ import 'widget_tester_extension.dart';
 
 void main() {
   group('end-to-end test', () {
-    testWidgets('verify EU login flow with email and password', (tester) async {
+    patrolTest('verify EU login flow with email and password',
+        (patrolTester) async {
+      final tester = patrolTester.tester;
       final emailSignupPage = EmailSignupTestPage(tester);
       final loginPage = LoginTestPage(tester);
       final permitBiometricPage = PermitBiometricTestPage(tester);
@@ -33,6 +36,34 @@ void main() {
       await permitBiometricPage.tapDenyButton();
 
       await homePage.verifyAppBarIsVisible();
+
+      if (await patrolTester.native
+          .isPermissionDialogVisible(timeout: const Duration(seconds: 5))) {
+        await patrolTester.native.grantPermissionWhenInUse();
+      }
+    });
+    testWidgets('part 2', (tester) async {
+      final emailSignupPage = EmailSignupTestPage(tester);
+      final loginPage = LoginTestPage(tester);
+      final permitBiometricPage = PermitBiometricTestPage(tester);
+      final homePage = HomeTestPage(tester);
+
+      await tester.startApp();
+
+      await Future.delayed(const Duration(seconds: 10));
+      await tester.makeSureLoadersHaveFinishedLoading();
+
+      await emailSignupPage.verifyEmailInputIsVisible();
+      await emailSignupPage.enterEmail('tamara+test3@givtapp.net');
+      await emailSignupPage.selectCountry('United States');
+      await emailSignupPage.tapContinueButton();
+
+      await loginPage.verifyPasswordInputIsVisible();
+      await loginPage.enterPassword('Welkom123');
+      await loginPage.tapLoginButton();
+
+      await permitBiometricPage.verifyDenyButtonIsVisible();
+      await permitBiometricPage.tapDenyButton();
     });
   });
 }
--- integration_test/features/email_signup/presentation/pages/email_signup_test_page.dart
+++ integration_test/features/email_signup/presentation/pages/email_signup_test_page.dart
@@ -13,11 +13,19 @@ class EmailSignupTestPage {
   final Finder emailInput = find.byKey(const ValueKey('Email-Input'));
   final Finder continueButton = find.byKey(const ValueKey('Email-Continue-Button'));
   final Finder scrollable = find.byKey(const ValueKey('Email-Signup-Scrollable'));
+  final Finder countryDropdown = find.byKey(const ValueKey('Country-Dropdown'));
 
   // Actions
   Future<void> enterEmail(String email) async {
     await tester.scrollToWidgetViaKeyValues('Email-Input', 'Email-Signup-Scrollable');
     await tester.enterText(emailInput, email);
+    await tester.pumpAndSettle();
+  }
+
+  Future<void> selectCountry(String country) async {
+    await tester.tap(countryDropdown);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text(country));
     await tester.pumpAndSettle();
   }
 
