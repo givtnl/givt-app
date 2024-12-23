de09c2c7b0dc416e8ed791b797997341d4ac7f0f
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2024-12-23 15:28:33 +0100

    Check device volume on roles screen (kids-1828)

---
+++ lib/features/family/features/background_audio/presentation/volume_bottomsheet.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:givt_app/core/enums/enums.dart';
+import 'package:givt_app/features/family/features/background_audio/presentation/animated_speaker.dart';
+import 'package:givt_app/features/family/shared/design/components/components.dart';
+import 'package:givt_app/features/family/utils/utils.dart';
+import 'package:givt_app/shared/models/analytics_event.dart';
+
+class VolumeBottomsheet extends StatelessWidget {
+  const VolumeBottomsheet({super.key, this.onReady});
+  final VoidCallback? onReady;
+  @override
+  Widget build(BuildContext context) {
+    return FunBottomSheet(
+      title: 'Turn up your volume for a better experience',
+      content: const SizedBox.shrink(),
+      icon: Container(
+        height: 112,
+        width: 112,
+        decoration: const BoxDecoration(
+          color: FamilyAppTheme.primary95,
+          shape: BoxShape.circle,
+        ),
+        child: const AnimatedSpeaker(
+          height: 48,
+        ),
+      ),
+      primaryButton: FunButton(
+        text: 'Ready',
+        onTap: () {
+          Navigator.of(context).pop();
+          onReady?.call();
+        },
+        analyticsEvent: AnalyticsEvent(
+          AmplitudeEvents.volumeBottomSheetReadyClicked,
+        ),
+      ),
+    );
+  }
+}
