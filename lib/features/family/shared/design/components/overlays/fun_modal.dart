05ba7cc31cc2f80d37065dc20ca7e4c7d72709a9
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2025-01-10 16:05:26 +0100

    add import

--- lib/features/family/shared/design/components/overlays/fun_modal.dart
+++ lib/features/family/shared/design/components/overlays/fun_modal.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/core/enums/amplitude_events.dart';
