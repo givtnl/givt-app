f43bbf38f36aa77b54e7f9aa39438d6fe2d3191b
Daniela Damaschin <danieladamaschin@Danielas-MacBook-Pro.local>
2025-01-30 12:49:07 +0200

    improve widgets

--- lib/features/family/features/parent_giving_flow/presentation/pages/parent_amount_page.dart
+++ lib/features/family/features/parent_giving_flow/presentation/pages/parent_amount_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:givt_app/core/enums/amplitude_events.dart';
-import 'package:givt_app/core/enums/country.dart';
 import 'package:givt_app/features/family/features/auth/bloc/family_auth_cubit.dart';
 import 'package:givt_app/features/family/features/auth/presentation/models/family_auth_state.dart';
 import 'package:givt_app/features/family/features/creditcard_setup/pages/credit_card_details.dart';
@@ -47,7 +45,6 @@ class _ParentAmountPageState extends State<ParentAmountPage> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: FunTopAppBar.primary99(
         title: 'Give',
@@ -57,167 +54,125 @@ class _ParentAmountPageState extends State<ParentAmountPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Container(
-              height: size.height,
-              width: size.width,
-              color: Colors.white,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (size.height > 780) widget.icon,
-                      const SizedBox(height: 16),
-                      TitleLargeText(
-                        widget.organisationName,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 8),
-                      const BodyMediumText(
-                        'How much would you like to give?',
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 16),
-                      DisplayMediumText(
-                        '${widget.currency} $_amount',
-                        textAlign: TextAlign.center,
-                        color: isActive
-                            ? FamilyAppTheme.primary40
-                            : FamilyAppTheme.neutral70,
-                      ),
-                      SizedBox(
-                        height:
-                            FunNumericKeyboard.getHeight(context) + 118 + 54,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              child: keyboard(),
-            ),
+            _contentColumn(),
+            _inputBottomColumn(),
           ],
         ),
       ),
     );
   }
 
-  Widget presetRow() => Container(
-        height: 54,
+  Widget _contentColumn() => Container(
+        height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Colors.grey[200],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            presetButton(25),
-            presetButton(50),
-            presetButton(75),
-            presetButton(100),
-          ],
-        ),
-      );
-  Widget presetButton(int amount) => InkWell(
-        onTap: () {
-          setState(() {
-            _amount = amount;
-            isActive = true;
-            isPresetAmount = true;
-          });
-        },
-        child: Container(
-          width: MediaQuery.sizeOf(context).width / 4 - 16,
-          height: 44,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(25),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (MediaQuery.of(context).size.height > 780) widget.icon,
+                const SizedBox(height: 16),
+                TitleLargeText(
+                  widget.organisationName,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                const BodyMediumText(
+                  'How much would you like to give?',
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                DisplayMediumText(
+                  '${widget.currency} $_amount',
+                  textAlign: TextAlign.center,
+                  color: isActive
+                      ? FamilyAppTheme.primary40
+                      : FamilyAppTheme.neutral70,
+                ),
+                SizedBox(
+                  height: FunNumericKeyboard.getHeight(context) + 118 + 54,
+                ),
+              ],
             ),
           ),
-          child: Center(
-            child: LabelSmallText.secondary40(
-              '\$$amount',
-            ),
-          ),
         ),
       );
-
-  Widget keyboard() => SizedBox(
-        height: FunNumericKeyboard.getHeight(context) + 118 + 54,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            SizedBox(
-              height: 118,
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.all(24),
-                child: FunButton(
-                  onTap: widget.authcheck
-                      ? () async {
-                          await FamilyAuthUtils.authenticateUser(
-                            context,
-                            checkAuthRequest: FamilyCheckAuthRequest(
-                              navigate: (context) async {
-                                _checkCardDetailsAndNavigate(context);
-                              },
-                            ),
-                          );
-                        }
-                      : () {
-                          _checkCardDetailsAndNavigate(context);
-                        },
-                  text: 'Give',
-                  isDisabled: !isActive,
-                  analyticsEvent: AnalyticsEvent(
-                    AmplitudeEvents.parentGiveClicked,
+  Widget _inputBottomColumn() => Positioned(
+        bottom: 0,
+        child: SizedBox(
+          height: FunNumericKeyboard.getHeight(context) + 118 + 54,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: 118,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: FunButton(
+                    onTap: widget.authcheck
+                        ? () async {
+                            await FamilyAuthUtils.authenticateUser(
+                              context,
+                              checkAuthRequest: FamilyCheckAuthRequest(
+                                navigate: (context) async {
+                                  _checkCardDetailsAndNavigate(context);
+                                },
+                              ),
+                            );
+                          }
+                        : () {
+                            _checkCardDetailsAndNavigate(context);
+                          },
+                    text: 'Give',
+                    isDisabled: !isActive,
+                    analyticsEvent: AnalyticsEvent(
+                      AmplitudeEvents.parentGiveClicked,
+                    ),
                   ),
                 ),
               ),
-            ),
-            presetRow(),
-            FunNumericKeyboard(
-              currencySymbol: Country.us.currency,
-              textColor: FamilyAppTheme.secondary40,
-              rightIcon: const Icon(
-                FontAwesomeIcons.deleteLeft,
-                color: FamilyAppTheme.secondary40,
-              ),
-              onPresetTap: (amount) {
-                setState(() {
-                  _amount = int.parse(amount);
-                  isActive = true;
-                });
-              },
-              onKeyboardTap: (value) {
-                setState(() {
-                  if (isActive) {
-                    if (isPresetAmount) {
-                      _amount = int.parse(value);
-                    } else {
-                      _amount = _amount * 10 + int.parse(value);
-                    }
-                  } else {
+              FunNumericKeyboard(
+                presets: const [25, 50, 75, 100],
+                onPresetTap: (amount) {
+                  setState(() {
+                    _amount = int.parse(amount);
                     isActive = true;
-                    _amount = int.parse(value);
-                  }
-                });
-              },
-              rightButtonFn: () {
-                setState(() {
-                  _amount = _amount ~/ 10;
-                  if (_amount < 2) {
-                    isActive = false;
-                  }
-                });
-              },
-              hideLeftButton: true,
-            ),
-          ],
+                    isPresetAmount = true;
+                  });
+                },
+                onKeyboardTap: (value) {
+                  setState(() {
+                    if (isActive) {
+                      // overwriting a previously selected preset
+                      if (isPresetAmount) {
+                        _amount = int.parse(value);
+                        isPresetAmount = false;
+                      } else {
+                        // normal typing
+                        _amount = _amount * 10 + int.parse(value);
+                      }
+                    } else {
+                      // overwriting the placeholder amount
+                      isActive = true;
+                      _amount = int.parse(value);
+                    }
+                  });
+                },
+                rightButtonFn: () {
+                  setState(() {
+                    _amount = _amount ~/ 10;
+                    if (_amount < 2) {
+                      isActive = false;
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       );
 
--- lib/features/family/shared/design/components/input/fun_numeric_keyboard.dart
+++ lib/features/family/shared/design/components/input/fun_numeric_keyboard.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:givt_app/core/enums/country.dart';
-import 'package:givt_app/features/amount_presets/models/models.dart';
-import 'package:givt_app/features/auth/cubit/auth_cubit.dart';
-import 'package:givt_app/utils/app_theme.dart';
-import 'package:givt_app/utils/util.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:givt_app/features/family/shared/widgets/texts/shared_texts.dart';
+import 'package:givt_app/features/family/utils/family_app_theme.dart';
 
 typedef KeyboardTapCallback = void Function(String text);
 
@@ -12,49 +9,22 @@ class FunNumericKeyboard extends StatefulWidget {
   const FunNumericKeyboard({
     required this.onKeyboardTap,
     required this.onPresetTap,
-    required this.currencySymbol,
+    required this.rightButtonFn,
     super.key,
     this.presets = const [],
-    this.textColor = Colors.black54,
-    this.rightButtonFn,
-    this.rightIcon = const Icon(
-      Icons.backspace_outlined,
-      color: Colors.black54,
-    ),
-    this.leftButtonFn,
-    this.hideLeftButton = false,
-    this.mainAxisAlignment = MainAxisAlignment.spaceEvenly,
   });
-
-  /// Color of the text [default = Colors.black]
-  final Color textColor;
-
-  /// Display a custom right icon
-  final Icon? rightIcon;
-
-  /// Action to trigger when right button is pressed
-  final VoidCallback? rightButtonFn;
-
-  /// Action to trigger when left button is pressed
-  final VoidCallback? leftButtonFn;
-
-  /// Hide the left button
-  final bool hideLeftButton;
 
   /// Callback when an item is pressed
   final KeyboardTapCallback onKeyboardTap;
 
-  /// Main axis alignment [default = MainAxisAlignment.spaceEvenly]
-  final MainAxisAlignment mainAxisAlignment;
+  /// Action to trigger when right button is pressed
+  final VoidCallback? rightButtonFn;
 
   /// Preset amounts
-  final List<Preset> presets;
+  final List<int> presets;
 
   /// Callback when a preset is tapped
   final KeyboardTapCallback onPresetTap;
-
-  /// Currency symbol for presets
-  final String currencySymbol;
 
   static double getHeight(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
@@ -74,21 +44,11 @@ class FunNumericKeyboard extends StatefulWidget {
 }
 
 class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
-  String _comma = ',';
-
   @override
   Widget build(BuildContext context) {
-    final countryCode = context.read<AuthCubit>().state.user.country;
-
-    // US & UK should have a . instead ,
-    if (countryCode == Country.us.countryCode ||
-        Country.unitedKingdomCodes().contains(countryCode)) {
-      _comma = '.';
-    }
-
     final size = MediaQuery.sizeOf(context);
     return Container(
-      padding: const EdgeInsets.all(8), //y
+      padding: const EdgeInsets.all(8),
       width: size.width,
       decoration: BoxDecoration(
         color: Colors.grey.shade200,
@@ -100,37 +60,11 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
       alignment: Alignment.center,
       child: Row(
         children: [
-          Visibility(
-            visible: widget.presets.isNotEmpty,
-            child: SizedBox(
-              width: size.width * 0.3,
-              child: Padding(
-                padding: const EdgeInsets.all(4), //y
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: widget.presets.map(
-                    (preset) {
-                      final amount = Util.formatNumberComma(
-                        preset.amount,
-                        Country.fromCode(countryCode),
-                      );
-                      return _buildPresetAmount(
-                        preset: amount,
-                        onTap: () => widget.onPresetTap(
-                          amount,
-                        ),
-                      );
-                    },
-                  ).toList(),
-                ),
-              ),
-            ),
-          ),
           Expanded(
             child: Column(
               children: [
+                _presetsRow(widget.presets),
                 Row(
-                  mainAxisAlignment: widget.mainAxisAlignment,
                   children: [
                     _calcButton('1'),
                     _calcButton('2'),
@@ -138,7 +72,6 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
                   ],
                 ),
                 Row(
-                  mainAxisAlignment: widget.mainAxisAlignment,
                   children: [
                     _calcButton('4'),
                     _calcButton('5'),
@@ -146,7 +79,6 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
                   ],
                 ),
                 Row(
-                  mainAxisAlignment: widget.mainAxisAlignment,
                   children: [
                     _calcButton('7'),
                     _calcButton('8'),
@@ -154,22 +86,21 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
                   ],
                 ),
                 Row(
-                  mainAxisAlignment: widget.mainAxisAlignment,
                   children: [
-                    if (widget.hideLeftButton)
-                      _calcButton(
-                        '',
-                      )
-                    else
-                      _calcButton(
-                        _comma,
-                        onTap: widget.leftButtonFn,
-                      ),
+                    _calcButton(
+                      '',
+                    ),
                     _calcButton('0'),
                     _calcButton(
                       '⌫',
                       onTap: widget.rightButtonFn,
-                      child: widget.rightIcon,
+                      child: const Padding(
+                        padding: EdgeInsets.all(2),
+                        child: Icon(
+                          FontAwesomeIcons.deleteLeft,
+                          color: FamilyAppTheme.secondary40,
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -188,32 +119,30 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
   }) {
     final size = MediaQuery.sizeOf(context);
 
-    var fontSize = 18.0; //y
+    var textScaler = 1.0;
     if (size.height < 600 && widget.presets.isNotEmpty) {
-      fontSize = fontSize * 0.9;
+      textScaler = 0.9;
     }
     final onButtonTap = onTap ?? () => widget.onKeyboardTap(value);
     final valueWidget = child ??
-        Text(
+        LabelLargeText(
           value,
           textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                fontWeight: FontWeight.bold,
-                color: widget.textColor,
-                fontSize: fontSize,
-              ),
+          color: FamilyAppTheme.secondary40,
+          textScaler: TextScaler.linear(textScaler),
         );
-    var padding = 14.0; //y
+
+    var padding = 8.0;
     if (size.height < 700) {
-      padding = 7.5;
+      padding = 4;
     }
     return Expanded(
       child: InkWell(
         borderRadius: BorderRadius.circular(45),
         onTap: onButtonTap,
         child: Container(
-          margin: EdgeInsets.all(size.height < 600 ? 2 : 4), //y
-          padding: EdgeInsets.all(padding), //y
+          margin: EdgeInsets.all(size.height < 600 ? 2 : 4),
+          padding: EdgeInsets.all(padding),
           alignment: Alignment.center,
           decoration: BoxDecoration(
             color: value.isEmpty ? Colors.grey.shade200 : Colors.white,
@@ -227,40 +156,32 @@ class _FunNumericKeyboardState extends State<FunNumericKeyboard> {
     );
   }
 
-  Widget _buildPresetAmount({
-    required String preset,
-    required VoidCallback onTap,
-  }) {
-    final size = MediaQuery.sizeOf(context);
-    var padding = 14.0;
-    if (size.height < 700) {
-      padding = 10;
-    }
-
-    var fontSize = 18.0;
-    if (size.height < 600 && widget.presets.isNotEmpty) {
-      fontSize = fontSize * 0.8;
-    }
-
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.fromLTRB(2, padding, 2, padding),
-        margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: AppTheme.presetsButtonColor,
-          borderRadius: BorderRadius.circular(45),
+  Widget _presetsRow(List<int> presets) => Container(
+        color: Colors.grey[200],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: presets.map(_presetButton).toList(),
         ),
-        child: Text(
-          '${widget.currencySymbol} $preset',
-          style: TextStyle(
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
+      );
+
+  Widget _presetButton(int amount) => InkWell(
+        onTap: () => widget.onPresetTap(amount.toString()),
+        child: Container(
+          width: MediaQuery.sizeOf(context).width / 4 - 12,
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.all(4),
+          decoration: const BoxDecoration(
             color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          child: Center(
+            child: LabelSmallText.secondary40(
+              '\$$amount',
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
