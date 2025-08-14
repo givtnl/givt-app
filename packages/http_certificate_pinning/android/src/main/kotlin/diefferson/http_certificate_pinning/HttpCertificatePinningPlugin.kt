3cd58c202313db615a03cc2b491560a21c989af0
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2025-08-14 10:10:36 +0200

    remove import

--- packages/http_certificate_pinning/android/src/main/kotlin/diefferson/http_certificate_pinning/HttpCertificatePinningPlugin.kt
+++ packages/http_certificate_pinning/android/src/main/kotlin/diefferson/http_certificate_pinning/HttpCertificatePinningPlugin.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.IOException
 import java.net.SocketTimeoutException
 import java.net.URL
