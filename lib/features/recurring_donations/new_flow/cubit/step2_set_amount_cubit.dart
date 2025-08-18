e748ced26be6664ed42c83d0f1869da2bc45038c
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2025-08-18 12:24:00 +0200

    use model filue

--- lib/features/recurring_donations/new_flow/cubit/step2_set_amount_cubit.dart
+++ lib/features/recurring_donations/new_flow/cubit/step2_set_amount_cubit.dart
@@ -3,43 +3,6 @@ import 'package:givt_app/features/recurring_donations/new_flow/repository/recurr
 import 'package:givt_app/shared/bloc/base_state.dart';
 import 'package:givt_app/shared/bloc/common_cubit.dart';
 
-class SetAmountUIModel extends Equatable {
-  const SetAmountUIModel({
-    this.selectedFrequency,
-    this.amount = '',
-    this.isLoading = false,
-    this.error,
-  });
-
-  final String? selectedFrequency;
-  final String amount;
-  final bool isLoading;
-  final String? error;
-
-  bool get isContinueEnabled =>
-      (selectedFrequency != null && selectedFrequency!.isNotEmpty) &&
-      amount.isNotEmpty &&
-      double.tryParse(amount) != null &&
-      double.parse(amount) > 0;
-
-  SetAmountUIModel copyWith({
-    String? selectedFrequency,
-    String? amount,
-    bool? isLoading,
-    String? error,
-  }) {
-    return SetAmountUIModel(
-      selectedFrequency: selectedFrequency ?? this.selectedFrequency,
-      amount: amount ?? this.amount,
-      isLoading: isLoading ?? this.isLoading,
-      error: error ?? this.error,
-    );
-  }
-
-  @override
-  List<Object?> get props => [selectedFrequency, amount, isLoading, error];
-}
-
 enum SetAmountAction {
   navigateToDuration,
 }
