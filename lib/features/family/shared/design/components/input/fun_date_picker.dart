f4ac61abe0fe24f12f8108a2435f30af2552e869
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2025-08-22 20:17:54 +0200

    start date + 1 day

--- lib/features/family/shared/design/components/input/fun_date_picker.dart
+++ lib/features/family/shared/design/components/input/fun_date_picker.dart
@@ -20,7 +20,7 @@ class FunDatePicker extends StatelessWidget {
 
     return GestureDetector(
       onTap: () async {
-        final now = DateTime.now();
+        final now = DateTime.now().add(const Duration(days: 1));
         final picked = await showDatePicker(
           context: context,
           initialDate: selectedDate ?? now,
