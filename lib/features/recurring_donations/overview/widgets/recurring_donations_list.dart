97170c3f0a758db0591bf0d75a773ef712845329
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2026-06-09 00:25:30 +0200

    improve padding

--- lib/features/recurring_donations/overview/widgets/recurring_donations_list.dart
+++ lib/features/recurring_donations/overview/widgets/recurring_donations_list.dart
@@ -32,7 +32,7 @@ class RecurringDonationsList extends StatelessWidget {
     }
 
     return ListView.separated(
-      padding: const EdgeInsets.only(bottom: 100),
+      padding: const EdgeInsets.only(bottom: 98), // inset + button size
       itemCount: donations.length,
       separatorBuilder: (context, index) => const SizedBox(height: 16),
       itemBuilder: (context, index) {
